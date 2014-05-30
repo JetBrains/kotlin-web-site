3d6374aff815b3f36e54c2e1b0bc5f737abb54d5
Stas Kurilov <stas.kurilov@jetbrains.com>
2014-05-30 12:48:31 +0400

    Bootstrapping js stuff added

---
+++ js/config/bootstrap.js
@@ -0,0 +1,21 @@
+require._config({
+    baseUrl: 'js',
+
+    paths: {
+        'jquery': 'vendor/jquery/jquery-1.11.0.min',
+        'jcarousel': 'vendor/jcarousel/jquery.jcarousel.min'
+    },
+
+    shim: {
+        'jcarousel': {
+            deps: ['jquery'],
+            exports: 'jQuery.fn.jcarousel'
+        }
+    },
+
+    deps: ['require'],
+
+    callback: function (require) {
+        require(['jquery'])
+    }
+});
