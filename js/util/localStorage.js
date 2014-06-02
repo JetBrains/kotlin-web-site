7a4a4e58b8cacb6e71a9892f24bcb1b5a153655d
Stas Kurilov <stas.kurilov@jetbrains.com>
2014-06-02 19:28:15 +0400

    Local storage improvements

--- js/util/localStorage.js
+++ js/util/localStorage.js
@@ -1,41 +1,49 @@
-define(function() {
-    var storage = {
-        setItem: function (key, originValue) {
-            var value = originValue,
-                type = typeof originValue;
+define({
+    setItem: function (key, value) {
+        var type = typeof value;
 
-            if (type !== 'string' || type !== 'number' || type !== 'boolean' || type === null) {
-                value = JSON.stringify(originValue);
-            }
-
-            localStorage.setItem(key, value);
-
-            return originValue;
-        },
-
-        getItem: function (key) {
-            var value = localStorage.getItem(key),
-                valueLength,
-                isNeedToParse;
-
-            if (value !== null) {
-                valueLength = value.length;
-                isNeedToParse =
-                    (value.substring(0, 1) === '[' && value.substring(valueLength-1, valueLength) === ']') ||
-                    (value.substring(0, 1) === '{' && value.substring(valueLength-1, valueLength) === '}');
-
-                if (isNeedToParse) {
-                    value = JSON.parse(value);
-                }
-            }
-
-            return value;
-        },
-
-        removeItem: function (key) {
-            localStorage.removeItem(key);
+        if (type !== 'string' || type !== 'number' || type !== 'boolean' || type === null) {
+            value = JSON.stringify(value);
         }
-    };
 
-    return storage;
+        localStorage.setItem(key, value);
+
+        return arguments[1];
+    },
+
+    getItem: function (key) {
+        var value = localStorage.getItem(key),
+            valueLength,
+            isNeedToParse;
+
+        if (value !== null) {
+            valueLength = value.length;
+            isNeedToParse =
+                (value.substring(0, 1) === '[' && value.substring(valueLength - 1, valueLength) === ']') ||
+                    (value.substring(0, 1) === '{' && value.substring(valueLength - 1, valueLength) === '}');
+
+            if (isNeedToParse) {
+                value = JSON.parse(value);
+            }
+        }
+
+        return value;
+    },
+
+    removeItem: function (key) {
+        localStorage.removeItem(key);
+    },
+
+    getAllItems: function () {
+        var key,
+            items = {},
+            i = 0;
+
+        for (key in localStorage) {
+            items[key] = localStorage[key];
+            i++;
+        }
+
+        return i > 0 ? items : null;
+    }
 });
