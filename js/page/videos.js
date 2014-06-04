6c17861ed52de608d19e76a4d0807d10531e306a
Stas Kurilov <stas.kurilov@jetbrains.com>
2014-06-04 23:04:56 +0400

    Added video description support

--- js/page/videos.js
+++ js/page/videos.js
@@ -1,47 +1,10 @@
-define(['jquery', 'com/nav-tree', 'com/video-player'], function($, NavTree, Player) {
+define(['com/video-gallery'], function(VideoGallery) {
 
     return function(data) {
-
-        NavTree.prototype.templates.leafItem = function(item) {
-            var hasUrl = 'url' in item,
-                hasDuration = 'duration' in item;
-
-            var t =
-                ['.tree-item.tree-leaf.js-item.js-leaf.video-item',
-                    [
-                    (hasUrl ? 'a' : 'div') + '.tree-item-title.tree-leaf-title.js-item-title.js-leaf-title.video-item-title', hasUrl ? {href: item.url} : null,
-                        ['span.marker'],
-                        ['span.text', item.title],
-                    hasDuration
-                        ? ['span.duration', item.duration]
-                        : null
-                    ]
-                ];
-
-            return t;
-        };
-
-        var tree,
-            player;
-
-        tree = new NavTree(data.elem, {data: data.videos});
-
-        player = new Player('video-player', {
-            width: '100%',
-            height: 480,
-            videoId: 'viiDaLpPfN4'
-        });
-
-        tree.on('selectLeaf', function(e, branch, elem) {
-            var videoUrl = elem.href,
-                videoId;
-
-            videoId = Player.getVideoIdFromUrl(videoUrl);
-            player.playVideo(videoId);
-        });
-
-        $(data.elem).find('a').on('click', function(e) {
-            e.preventDefault();
+        new VideoGallery(data.elem, {
+            playerElem: data.playerElem,
+            descriptionElem: data.descriptionElem,
+            data: data.videos
         });
     };
 
