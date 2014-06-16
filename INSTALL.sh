b3e5f0d50f9505f4b1108cc212e723b0693cafa6
Stas Kurilov <stas.kurilov@jetbrains.com>
2014-06-16 20:44:31 +0400

    Update install script

--- INSTALL.sh
+++ INSTALL.sh
@@ -3,6 +3,7 @@ RUBY_VERSION_=`ruby -e "puts RUBY_VERSION"`
 BUNDLER_VERSION_=1.6.2
 GEM_LOCAL_PATH_=~/.gem/ruby/$RUBY_VERSION_/gems
 LOCAL_BUNDLER_PATH_=$GEM_LOCAL_PATH_/bundler-$BUNDLER_VERSION_
+BUNDLER_PATH=`which bundle` || $LOCAL_BUNDLER_PATH_/bin/bundle
 
 if which ruby >/dev/null && which gem >/dev/null; then
 
@@ -11,8 +12,11 @@ if which ruby >/dev/null && which gem >/dev/null; then
         exit 0
     fi
 
-    ARCHFLAGS=-Wno-error=unused-command-line-argument-hard-error-in-future gem install bundler --version $BUNDLER_VERSION_ --user-install --no-document
-    $LOCAL_BUNDLER_PATH_/bin/bundle install --path vendor/bundle
+    if [[ `which bundle` == "" ]]; then
+        ARCHFLAGS=-Wno-error=unused-command-line-argument-hard-error-in-future gem install bundler --version $BUNDLER_VERSION_ --user-install --no-document
+    fi
+
+    $BUNDLER_PATH install
 else
     echo "You need Ruby (>= 1.9.3) and rubygems to get it working"
 fi
