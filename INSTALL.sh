f3e2adbf186163dbf68acc7b588468338fe2a1f1
Stas Kurilov <stas.kurilov@jetbrains.com>
2014-07-07 16:05:33 +0400

    Install script improvements

--- INSTALL.sh
+++ INSTALL.sh
@@ -3,7 +3,7 @@ RUBY_VERSION_=`ruby -e "puts RUBY_VERSION"`
 BUNDLER_VERSION_=1.6.2
 GEM_LOCAL_PATH_=~/.gem/ruby/$RUBY_VERSION_/gems
 LOCAL_BUNDLER_PATH_=$GEM_LOCAL_PATH_/bundler-$BUNDLER_VERSION_
-BUNDLER_PATH=`which bundle` || $LOCAL_BUNDLER_PATH_/bin/bundle
+BUNDLER_PATH=`which bundle` || "$LOCAL_BUNDLER_PATH_/bin/bundle"
 
 if which ruby >/dev/null && which gem >/dev/null; then
 
@@ -12,8 +12,8 @@ if which ruby >/dev/null && which gem >/dev/null; then
         exit 0
     fi
 
-    if [[ `which bundle` == "" ]]; then
-        ARCHFLAGS=-Wno-error=unused-command-line-argument-hard-error-in-future gem install bundler --version $BUNDLER_VERSION_ --user-install --no-document
+    if [ ! -f $BUNDLER_PATH ]; then
+       ARCHFLAGS=-Wno-error=unused-command-line-argument-hard-error-in-future gem install bundler --version $BUNDLER_VERSION_ --user-install --no-document
     fi
 
     $BUNDLER_PATH install --path vendor/bundle
