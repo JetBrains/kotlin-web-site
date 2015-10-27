0e8e8dd2c3789f9f822d6324820deb681624d752
Dmitry Jemerov <yole@jetbrains.com>
2015-10-27 10:22:40 +0100

    reimport project, advance Kotlin version to 1.0 beta

--- code/tutorials/spring-boot-restful/src/main/kotlin/kotlin/demo/Application.kt
+++ code/tutorials/spring-boot-restful/src/main/kotlin/kotlin/demo/Application.kt
@@ -1,7 +1,7 @@
 package kotlin.demo
+
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-
 
 @SpringBootApplication
 open class Application {
