a77868cf5f96c3d0874b0a5fec1d181f82ed01cb
Frank Smith <oryx.gazella@gmail.com>
2015-10-31 00:43:02 +0200

    Update spring-boot kotlin tutorial with changes for kotlin beta.
    Consistently remove the redundant public modifiers everywhere.

--- code/tutorials/spring-boot-restful/src/main/kotlin/kotlin/demo/Greeting.kt
+++ code/tutorials/spring-boot-restful/src/main/kotlin/kotlin/demo/Greeting.kt
@@ -1,3 +1,3 @@
 package kotlin.demo
 
-data public class Greeting(val id: Long, val content: String)
+data class Greeting(val id: Long, val content: String)
--- code/tutorials/spring-boot-restful/src/main/kotlin/kotlin/demo/GreetingController.kt
+++ code/tutorials/spring-boot-restful/src/main/kotlin/kotlin/demo/GreetingController.kt
@@ -6,12 +6,12 @@ import org.springframework.web.bind.annotation.RestController
 import java.util.concurrent.atomic.AtomicLong
 
 @RestController
-public class GreetingController {
+class GreetingController {
 
     val counter = AtomicLong()
 
     @RequestMapping("/greeting")
-    public fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String): Greeting {
+    fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String): Greeting {
         return Greeting(counter.incrementAndGet(), "Hello, $name")
     }
 }
