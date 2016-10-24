b21cac520256890c8d0228272f926dc55d7a1922
satamas <se.atamas@gmail.com>
2016-10-24 13:12:09 +0300

    Return anchors to headers.

--- static/js/page/tutorials.js
+++ static/js/page/tutorials.js
@@ -1,5 +1,16 @@
 define(['com/nav-tree'], function (NavTree) {
 
+    $(':header').each(function (ind, element) {
+        var id = element.getAttribute("id");
+        var tagName = element.tagName.toLowerCase();
+        if( id == null) return;
+        if(!(tagName == "h1" || tagName == "h2" || tagName == "h3")) return;
+        var referenceElement = document.createElement("a");
+        referenceElement.className = "anchor";
+        referenceElement.href = "#" + id;
+        element.appendChild(referenceElement)
+    });
+
     return (function () {
         $(document).ready(function () {
             new NavTree(document.getElementById('tutorials-nav'));
