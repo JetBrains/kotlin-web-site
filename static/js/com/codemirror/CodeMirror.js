ba8779380a9f655b12702a8ba04bed66f42f5870
satamas <se.atamas@gmail.com>
2016-12-22 14:00:58 +0300

    Minor: add missing file

---
+++ static/js/com/codemirror/CodeMirror.js
@@ -0,0 +1,10 @@
+const CodeMirror = require('codemirror');
+require('codemirror/addon/runmode/colorize.js');
+
+require('codemirror/mode/clike/clike.js');
+require('codemirror/mode/groovy/groovy.js');
+require('codemirror/mode/xml/xml.js');
+require('codemirror/mode/javascript/javascript.js');
+require('codemirror/mode/shell/shell.js');
+
+module.exports = CodeMirror;
