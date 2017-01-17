7b596f07957f5b15fcea60ca36af5404ce5646a8
satamas <se.atamas@gmail.com>
2017-01-17 14:53:42 +0300

    Process svg hovers

--- static/js/page/community/community.js
+++ static/js/page/community/community.js
@@ -1,21 +1,15 @@
 require('./community.scss');
 var $ = require('jquery');
 
-function getSvgId(element) {
-  var svgId = null;
-  element.classList.forEach(function (className) {
-    if (className.startsWith('_')) {
-      svgId = className.substring(1).replace(/-/g, '_')
-    }
-  });
-  return svgId;
+function processButtonHover(element, hover) {
+  setHoverState(element, hover);
 }
 
-function processSvgElementHover(element, hover) {
-  var svgId = getSvgId(element);
+function setHoverState(id, hover) {
   var $imgElement = $('.all-speak-kotlin_img');
-  $imgElement.find('#' + svgId + ' .hover').toggle(hover);
-  $imgElement.find('#' + svgId + ' .default').toggle(!hover);
+  $("[data-svg-id='" + id + "'] > a").toggleClass('_hover', hover);
+  $imgElement.find('#' + id + ' .hover').toggle(hover);
+  $imgElement.find('#' + id + ' .default').toggle(!hover);
 }
 
 $(document).ready(function () {
@@ -24,6 +18,7 @@ $(document).ready(function () {
     dataType: 'xml'
   }).success(function (data) {
     var svgElement = data.documentElement;
+    var $imgElement = $('.all-speak-kotlin_img');
     $(svgElement).find('g').each(function (index, element) {
       var id = element.getAttribute('id');
       if (id == null) return;
@@ -36,12 +31,23 @@ $(document).ready(function () {
         element.setAttribute('display', 'none');
       }
     });
-    $('.all-speak-kotlin_img').append(data.documentElement)
+    $imgElement.append(data.documentElement);
+
+    $imgElement.find('g.hover').parent().on('click', function () {
+      var href = $('[data-svg-id="' + this.getAttribute('id') + '"] > a').attr('href');
+      window.open(href);
+    }).on('mouseenter', function () {
+      setHoverState(this.getAttribute('id'), true)
+    }).on('mouseleave', function () {
+      setHoverState(this.getAttribute('id'), false)
+    })
   });
 
   $('.all-speak-kotlin_link-button').on('mouseenter', function () {
-    processSvgElementHover(this.parentNode, true)
+    setHoverState(this.parentNode.getAttribute('data-svg-id'), true)
   }).on('mouseleave', function () {
-    processSvgElementHover(this.parentNode, false)
+    setHoverState(this.parentNode.getAttribute('data-svg-id'), false)
   });
+
+
 });
