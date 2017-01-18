8b9824e8b2855aa1ca8516734630152d15679844
satamas <se.atamas@gmail.com>
2017-01-18 14:16:55 +0300

    Animation and a lot of other fixes on community page.

--- static/js/page/community/community.js
+++ static/js/page/community/community.js
@@ -1,4 +1,5 @@
 require('./community.scss');
+var AOS = require('aos');
 var $ = require('jquery');
 
 function setHoverState(id, hover) {
@@ -6,6 +7,21 @@ function setHoverState(id, hover) {
   $("[data-svg-id='" + id + "'] > a").toggleClass('_hover', hover);
   $imgElement.find('#' + id + ' .hover').toggle(hover);
   $imgElement.find('#' + id + ' .default').toggle(!hover);
+}
+
+function setAnimation(id, animation) {
+  var linkElement = document.querySelector('[data-svg-id="' + id + '"]');
+  var svgElement = document.getElementById(id);
+
+  if (linkElement) {
+    linkElement.setAttribute('data-aos', animation + "_link");
+    linkElement.setAttribute('data-aos-anchor', ".all-speak-kotlin");
+    linkElement.setAttribute('data-aos-anchor-placement', "center-center");
+  }
+
+  svgElement.setAttribute('data-aos', animation);
+  svgElement.setAttribute('data-aos-anchor', ".all-speak-kotlin");
+  svgElement.setAttribute('data-aos-anchor-placement', "center-center");
 }
 
 $(document).ready(function () {
@@ -36,14 +52,35 @@ $(document).ready(function () {
       setHoverState(this.getAttribute('id'), true)
     }).on('mouseleave', function () {
       setHoverState(this.getAttribute('id'), false)
-    })
+    });
+
+
+    $('.all-speak-kotlin_link-button').on('mouseenter', function () {
+      setHoverState(this.parentNode.getAttribute('data-svg-id'), true)
+    }).on('mouseleave', function () {
+      setHoverState(this.parentNode.getAttribute('data-svg-id'), false)
+    });
+
+    setAnimation("talking_kotlin", "anim1");
+    setAnimation("reddit", "anim2");
+    setAnimation("slack", "anim3");
+    setAnimation("linkedin", "anim4");
+    setAnimation("Layer_3", "anim5");
+    setAnimation("Layer_2", "anim5");
+    setAnimation("kotlin_talks", "anim5");
+    setAnimation("Layer_6", "anim6");
+    setAnimation("Layer_7", "anim7");
+    setAnimation("Layer_8", "anim8");
+    setAnimation("kotlin_forum", "anim9");
+    setAnimation("Layer_4", "anim9");
+    setAnimation("Layer_5", "anim9");
+    setAnimation("stackoverflow", "anim10");
+    setAnimation("twitter", "anim11");
+    setAnimation("google", "anim12");
+    setAnimation("klink", "anim13");
+
+    AOS.init({
+      duration: 500
+    });
   });
-
-  $('.all-speak-kotlin_link-button').on('mouseenter', function () {
-    setHoverState(this.parentNode.getAttribute('data-svg-id'), true)
-  }).on('mouseleave', function () {
-    setHoverState(this.parentNode.getAttribute('data-svg-id'), false)
-  });
-
-
 });
