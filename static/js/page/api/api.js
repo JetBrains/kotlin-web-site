f569de3157010e54a0be9e653d43e779a7382e63
satamas <se.atamas@gmail.com>
2017-03-01 11:38:24 +0300

    Support multiple versions/platforms in documentation.

---
+++ static/js/page/api/api.js
@@ -0,0 +1,123 @@
+require('./api.scss');
+const Dropdown = require('../../com/dropdown');
+const $ = require('jquery');
+
+function getVersion(element) {
+  let version = $(element).attr('data-kotlin-version');
+  if (version.startsWith("Kotlin ")) {
+    version = version.substring("Kotlin ".length)
+  }
+  return version
+}
+
+function updateState(state) {
+  const $platformDependentElements = $('[data-platform]');
+  const $versionDependentElements = $('[data-kotlin-version]');
+  $versionDependentElements.removeClass('hidden');
+  $platformDependentElements.removeClass('hidden');
+
+  if (state.platform != 'all') {
+    $platformDependentElements.each((ind, element) => {
+      const $element = $(element);
+      if ($element.attr('data-platform') == state.platform) return;
+      $element.addClass('hidden')
+    })
+  }
+
+  $versionDependentElements.each((ind, element) => {
+    const $element = $(element);
+    const version = getVersion(element);
+    if (version <= state.version) return;
+    $element.addClass('hidden')
+  })
+}
+
+function addSelectToPanel(panelElement, title, config) {
+  const selectElement = $(`<div class="api-panel__select"><span class="api-panel__dropdown-title">${title}</span></div>`);
+  $(panelElement).append(selectElement);
+  new Dropdown(selectElement, config);
+}
+
+function initializeSelects() {
+  const $breadcrumbs = $('.api-docs-breadcrumbs');
+  if ($breadcrumbs.length > 0) {
+    $breadcrumbs
+      .wrap('<div class="api-page-panel"></div>')
+      .after('<div class="api-panel__switchers"></div>');
+  } else {
+    $('.page-content').prepend('<div class="api-page-panel"><div class="api-docs-breadcrumbs"></div><div class="api-panel__switchers"></div></div>');
+  }
+
+  const switchersPanel = $('.api-panel__switchers')[0];
+
+  const state = {
+    platform: 'all',
+    version: '1.1'
+  };
+
+  const platforms = {
+    'all': 'All'
+  };
+  $('[data-platform]').each((ind, element) => {
+    const platform = element.getAttribute('data-platform');
+    platforms[platform] = platform
+  });
+
+  if (Object.keys(platforms).length > 2) {
+    addSelectToPanel(switchersPanel, "Platform", {
+      items: platforms,
+      onSelect: (platform) => {
+        state.platform = platform;
+        updateState(state)
+      }
+    });
+  }
+
+
+  const versions = {
+    '1.0': '1.0'
+  };
+  $('[data-kotlin-version]').each((ind, element) => {
+    const version = getVersion(element);
+    versions[version] = version
+  });
+
+  if (Object.keys(versions).length > 1) {
+    addSelectToPanel(switchersPanel, "Version", {
+      items: versions,
+      selectedIndex: 1,
+      onSelect: (version) => {
+        state.version = version;
+        updateState(state)
+      }
+    })
+  }
+}
+
+function addTag(rowElement, tag) {
+  let $tagsElement = $(rowElement).find('.tags');
+  if ($tagsElement.length == 0) {
+    $tagsElement = $('<div class="tags"></div>');
+    $(rowElement).find('td:first').append($tagsElement);
+  }
+  $tagsElement.append(`<div class="tags__tag">${tag}</div>`)
+}
+
+function addTags() {
+  $('[data-platform]').each((ind, element) => {
+    const platform = element.getAttribute('data-platform');
+    addTag(element, platform)
+  });
+
+  $('[data-kotlin-version]').each((ind, element) => addTag(element, getVersion(element)));
+
+  $('[data-jre-version]').each((ind, element) => {
+    const platform = element.getAttribute('data-jre-version');
+    addTag(element, platform)
+  });
+}
+
+$(document).ready(() => {
+  initializeSelects();
+  addTags()
+});
