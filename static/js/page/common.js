10fa5ded4fa048e65e34d0b2eb0a244959c84ddb
Prendota <Prendota@mail.ru>
2019-03-19 10:07:52 +0300

    chore: temporary disable cookie banner

--- static/js/page/common.js
+++ static/js/page/common.js
@@ -3,7 +3,6 @@ import $ from 'jquery';
 import 'whatwg-fetch';
 
 import '../com/search/search';
-import '../com/cookie-banner';
 import '../com/cities-banners';
 import GifPlayer from '../com/gif-player/gif-player';
 import CodeMirror from '../com/codemirror/CodeMirror';
