d9ff95ca155099798b1e9de7ce3d0ac16f016227
Eugene Petrenko <eugene.petrenko@jetbrains.com>
2019-05-22 12:30:27 +0200

    fix GitHub link ('origin/') was extra, use new repository URL from Kotlin organization

--- pages-includes/docs/tutorials/native/generate-includes.sh
+++ pages-includes/docs/tutorials/native/generate-includes.sh
@@ -14,7 +14,7 @@ if [[ ! -d "$EXTERNAL" ]] ; then
 fi
 
 if [[ ! -d "$SAMPLES_REPO" ]] ; then
-   git clone https://github.com/JetBrains/kotlin-web-site-samples.git "$SAMPLES_REPO"
+   git clone https://github.com/kotlin/web-site-samples.git "$SAMPLES_REPO"
 else
    git "--git-dir=$SAMPLES_REPO/.git" fetch
 fi
@@ -23,13 +23,14 @@ function generate_code_block {
   code_target="$DIR/$1-code.md"
   link_target="$DIR/$1-link.md"
 
-  branch="origin/$2"
+  branch="$2"
+  remoteBranch="origin/$branch"
   lang=$3
   os=$4
   fileInBranch=$5
 
-  zipUrl=https://github.com/JetBrains/kotlin-web-site-samples/archive/$branch.zip
-  code="$(git "--git-dir=$SAMPLES_REPO/.git" show $branch:$fileInBranch)"
+  zipUrl=https://github.com/kotlin/web-site-samples/archive/$branch.zip
+  code="$(git "--git-dir=$SAMPLES_REPO/.git" show $remoteBranch:$fileInBranch)"
   code="$(echo "$code" | sed '/./,$!d' | sed -e :a -e '/^\n*$/{$d;N;};/\n$/ba')"
 
   echo ""                                                                          >> $code_target
@@ -45,7 +46,7 @@ function generate_code_block {
   echo ""                                                                          >> $code_target
 
   echo "<span class=\"multi-language-span\" data-lang=\"$lang\" data-os=\"$os\">"  >> $link_target
-  echo "[GitHub]($zipUrl)" >> $link_target
+  echo "[GitHub]($zipUrl)." >> $link_target
   echo "</span>" >> $link_target
 }
 
