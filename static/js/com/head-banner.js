0a33e909ac70cb1e47f9a1bbe10419d10603f1f4
Alexander Prendota <Prendota@mail.ru>
2020-04-02 20:22:16 +0300

    Revert "feat(banner): add info about Try obsolete"

--- static/js/com/head-banner.js
+++ static/js/com/head-banner.js
@@ -1,22 +1,19 @@
 import $ from "jquery";
 
-const BANNER_KEY = 'kotlin-global-banner';
+const BANNER_KEY = 'kotlin-kotlinconf-15-11-19';
 
-$(() => {
-  const $bannerContainer = $('.banner-container');
-  let bannerKey = $bannerContainer.data('banner-id') || BANNER_KEY;
-
-  const isClosed = window.localStorage.getItem(bannerKey);
-
+$(document).ready(() => {
+  const isClosed = window.localStorage.getItem(BANNER_KEY);
   if (!isClosed) {
-    const $banner = $bannerContainer.closest('.banner-nav');
-
-    $bannerContainer.on('click touch', $bannerContainer, function closeBanner() {
-      const $banner = $('.banner-nav');
-      $banner.css("display", "none");
-      window.localStorage.setItem(bannerKey, "true");
-    });
-
+    const $banner = $('.banner-nav');
     $banner.css("display", "block");
+    const searchButton = $('.close-head-banner');
+    searchButton.on('click touch', closeBanner);
+  }
+
+  function closeBanner() {
+    const $banner = $('.banner-nav');
+    $banner.css("display", "none");
+    window.localStorage.setItem(BANNER_KEY, "true");
   }
 });
