4c91a92aaef406de08da408152b72386b1b6107b
zoobestik <kb.chernenko@gmail.com>
2020-04-05 16:18:15 +0300

    chore: remove resources link

--- kotlin-website.py
+++ kotlin-website.py
@@ -208,7 +208,7 @@ def pdf():
 
 @app.route('/docs/resources.html')
 def resources():
-    return render_template('pages/resources.html')
+    return render_template('redirect.html', url="https://kotlin.link/")
 
 
 @app.route('/community/')
