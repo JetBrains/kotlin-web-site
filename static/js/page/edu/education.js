5d59282e0d0346b49911dbea918beb10961cc1b7
zoobestik <kb.chernenko@gmail.com>
2020-06-07 02:12:51 +0300

    fixup! feat; add edu page

--- static/js/page/edu/education.js
+++ static/js/page/edu/education.js
@@ -1,1 +1,24 @@
-import "./education.scss"
+import $ from 'jquery';
+import Map from "../events/map/Map";
+import store from "./store.json"
+
+import "./education.scss";
+
+$(function () {
+    const container = $('.edu-universities-top')[0];
+
+    if (container) {
+        const tag = document.createElement('div');
+        tag.className = "edu-universities-map";
+        tag.textContent = "Loading map...";
+        Map.create(tag, store);
+
+        const { parentNode, nextSibling } = container;
+
+        if (nextSibling) {
+            parentNode.insertBefore(tag, nextSibling);
+        } else {
+            parentNode.appendChild()
+        }
+    }
+});
