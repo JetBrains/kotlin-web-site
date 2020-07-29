3f5be4538d037ffbf1dda5ac3c2b93d3ec22b27f
Ekaterina.Volodko <ekaterina.volodko@jetbrains.com>
2020-07-29 19:27:17 +0300

    Revert adding a test file This reverts commit cff3f875

--- pages/docs/reference/test.kt
+++
@@ -1,8 +0,0 @@
-expect class AtomicRef<V>(value: V) {
-    fun get(): V
-    fun set(value: V)
-    fun getAndSet(value: V): V
-    fun compareAndSet(expect: V, update: V): Boolean
-}
-​
-actual typealias AtomicRef<V> = java.util.concurrent.atomic.AtomicReference<V>
