b53c2bc5b6d6e11b6a65914636cfbe096b1e5cbb
nikpachoo <nikpachoo@gmail.com>
2021-10-28 18:11:10 +0300

    ktl-491 chore: combine babel settings for server and client sides

--- scripts/react-renderer/compile.js
+++ scripts/react-renderer/compile.js
@@ -1,9 +1,18 @@
 /* eslint-disable import/no-dynamic-require,prefer-template,comma-dangle */
-const babelRc = Object.assign({}, require('./.babelrc.json'), {
-    cache: true,
-    extensions: ['.mjs', '.js', '.jsx'],
-    ignore: [],
-});
+
+const babelRc = {
+  extends: './../../.babelrc',
+  extensions: ['.mjs', '.js', '.jsx'],
+  ignore: [],
+  plugins: [
+    [
+      "babel-plugin-transform-import-ignore",
+      {
+        "patterns": [/\.s?css$/]
+      }
+    ]
+  ]
+};
 
 require('@babel/register')(babelRc);
 require('./compile.mjs')['default']();
