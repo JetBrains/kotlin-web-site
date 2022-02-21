b94de5ba5fcfe23254eb3476570f7abe7f5b7c58
Yuri Teplyakov <ronnie57x@gmail.com>
2022-02-21 11:15:23 +0300

    ktl-631 chore: review updates

--- scripts/generate-ktl-component.js
+++ scripts/generate-ktl-component.js
@@ -5,18 +5,22 @@ try {
   const componentName = process.argv[2];
   output = spawn('node', ['compile.js', componentName], {cwd: 'scripts/react-renderer'});
 
-  output.stdout.on('data', (data) => {
+  output.stdout.on('data', data => {
     const renderedHtml = data.toString();
 
     if (!fs.existsSync('generated')) {
       fs.mkdirSync('generated');
     }
 
-    fs.writeFileSync(`./generated/${componentName}.html`, renderedHtml, (e) => {
-      if (e) throw e;
-    });
+    fs.writeFileSync(`./generated/${componentName}.html`, renderedHtml)
 
   });
+
+  output.stderr.on('data', data => {
+    console.error(`stderr: ${data}`);
+  });
+
+
 } catch (e) {
   console.error(e);
   process.exit(1);
