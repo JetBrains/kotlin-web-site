b3cdf99d18306cf3a57ee3c3a245c0bbb5312ee0
nikpachoo <nikpachoo@gmail.com>
2022-04-11 16:07:39 +0300

    ktl-657 feat: init Next.js

---
+++ next.config.js
@@ -0,0 +1,16 @@
+const packageJSON = require('./package.json');
+
+const { withGlobalCss } = require('next-global-css');
+const withConfigCss = withGlobalCss();
+
+let transpiledPackages = [
+  '@jetbrains/kotlin-web-site-ui',
+  ...(Object.keys(packageJSON.dependencies).filter(it => it.includes('@rescui/')))
+];
+
+const withTranspile = require('next-transpile-modules')(transpiledPackages, {debug: true});
+
+module.exports = withTranspile(withConfigCss({
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  inlineImageLimit: 0
+}));
