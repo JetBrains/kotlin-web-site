824241afbf02a2f6cf98ab51910bc9d263e41f5b
nikpachoo <nikpachoo@gmail.com>
2022-06-02 14:48:24 +0200

    ktl-751 fix: Community pages fixes

--- blocks/community/overview-bottom-links/overview-bottom-link.tsx
+++ blocks/community/overview-bottom-links/overview-bottom-link.tsx
@@ -6,18 +6,18 @@ import Link from 'next/link';
 import styles from './overview-bottom-link.module.css';
 
 interface Props {
-    title: string;
+    children: React.ReactNode;
     buttonTitle: string;
     buttonLink: string;
 }
 
-export const OverviewBottomLink: FC<Props> = ({ title, buttonTitle, buttonLink }) => {
+export const OverviewBottomLink: FC<Props> = ({ children, buttonTitle, buttonLink }) => {
     return (
         <div className={styles.wrapper}>
             <div className="ktl-container">
                 <div className="ktl-row">
                     <div className="ktl-col">
-                        <h2 className={classnames('ktl-h1', styles.title)}>{title}</h2>
+                        <h2 className={classnames('ktl-h1', styles.title)}>{children}</h2>
                         <Link href={buttonLink}>
                             <Button size="l" mode="outline">
                                 {buttonTitle}
