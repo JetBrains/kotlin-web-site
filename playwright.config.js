ad139857f4da67899c52171cbacedc551143a2a2
nikpachoo <nikpachoo@gmail.com>
2022-06-21 13:23:25 +0200

    ktl-744 chore: run test in prod env

--- playwright.config.js
+++ playwright.config.js
@@ -4,8 +4,9 @@ const config = {
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   reporter: process.env.CI ? 'dot' : 'list',
+  maxFailures: process.env.CI ? 1 : 0,
   use: {
-    baseURL: 'http://localhost:9000',
+    baseURL: process.env.BASE_URL || 'http://localhost:9000',
     trace: 'on-first-retry'
   },
   projects: [
