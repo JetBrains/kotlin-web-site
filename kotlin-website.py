76cf932458bfbeed23313f58d656baae24da030a
nikpachoo <nikpachoo@gmail.com>
2022-07-01 09:56:43 +0200

    ktl-744 chore: remove obsolete Flask settings

--- kotlin-website.py
+++ kotlin-website.py
@@ -568,7 +568,4 @@ if __name__ == '__main__':
             print("Unknown argument: " + argv_copy[1])
             sys.exit(1)
     else:
-        app.run(host="0.0.0.0", port=8080, debug=True, threaded=True, **{"extra_files": {
-            '/src/data/_nav.yml',
-            *glob.glob("/src/pages-includes/**/*", recursive=True),
-        }})
+        app.run(host="0.0.0.0", port=8080, debug=True, threaded=True, use_debugger=False, use_reloader=False)
