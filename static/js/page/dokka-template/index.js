aac95bc3c01f048cd94dd5c4cd2aacc4fa53f35c
nikpachoo <nikpachoo@gmail.com>
2022-10-11 14:44:31 +0200

    ktl-696 feat: global nav in Dokka' template

--- static/js/page/dokka-template/index.js
+++ static/js/page/dokka-template/index.js
@@ -1,6 +1,7 @@
 import './index.scss';
 import { initComponent, ktlHelpers } from "../../ktl-component/ktl-helpers";
 import Footer from "./footer/index.jsx";
+import Header from "./header/index.jsx";
 
 document.addEventListener('DOMContentLoaded', init);
 
@@ -9,6 +10,9 @@ function init() {
     switch (name) {
       case 'footer':
         initComponent(node.nextElementSibling, Footer, props);
+        break;
+      case 'header':
+        initComponent(node.nextElementSibling, Header, props);
         break;
       default:
         console.error(`The "${name}" component was not found.`);
