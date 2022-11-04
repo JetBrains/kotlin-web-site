ebc3a2f7bd22b6f914b7e875cec2f19e0fd9db68
Yuri Teplyakov <ronnie57x@gmail.com>
2022-11-04 12:57:19 +0100

    ktl-868 feat: add datetime dsl configuration

--- .teamcity/builds/apiReferences/BuildApiReferencesProject.kt
+++ .teamcity/builds/apiReferences/BuildApiReferencesProject.kt
@@ -3,6 +3,9 @@ package builds.apiReferences
 import builds.apiReferences.kotlinx.coroutines.KotlinxCoroutinesBuildApiReference
 import builds.apiReferences.kotlinx.coroutines.KotlinxCoroutinesBuildSearchIndex
 import builds.apiReferences.kotlinx.coroutines.KotlinxCoroutinesPrepareDokkaTemplates
+import builds.apiReferences.kotlinx.datetime.KotlinxDatetimeBuildApiReference
+import builds.apiReferences.kotlinx.datetime.KotlinxDatetimeBuildSearchIndex
+import builds.apiReferences.kotlinx.datetime.KotlinxDatetimePrepareDokkaTemplates
 import builds.apiReferences.kotlinx.serialization.KotlinxSerializationBuildApiReference
 import builds.apiReferences.kotlinx.serialization.KotlinxSerializationBuildSearchIndex
 import builds.apiReferences.kotlinx.serialization.KotlinxSerializationPrepareDokkaTemplates
@@ -11,11 +14,16 @@ import builds.apiReferences.templates.BuildApiReferenceSearchIndex
 import builds.apiReferences.templates.PrepareDokkaTemplate
 import builds.apiReferences.vcsRoots.KotlinxCoroutines
 import builds.apiReferences.vcsRoots.KotlinxSerialization
+import builds.apiReferences.vcsRoots.KotlinxDatetime
 import jetbrains.buildServer.configs.kotlin.Project
 
 object BuildApiReferencesProject : Project({
   name = "API References"
   description = "Build API references for kotlinlang.org"
+
+  buildType(KotlinxDatetimeBuildApiReference)
+  buildType(KotlinxDatetimeBuildSearchIndex)
+  buildType(KotlinxDatetimePrepareDokkaTemplates)
 
   buildType(KotlinxSerializationBuildApiReference)
   buildType(KotlinxSerializationBuildSearchIndex)
@@ -27,6 +35,7 @@ object BuildApiReferencesProject : Project({
 
   vcsRoot(KotlinxSerialization)
   vcsRoot(KotlinxCoroutines)
+  vcsRoot(KotlinxDatetime)
 
   template(PrepareDokkaTemplate)
   template(BuildApiReference)
