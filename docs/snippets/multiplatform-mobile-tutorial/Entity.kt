abf83add17b693c42f562aee357dfb6d4f72d33c
Danil Pavlov <danil.pavlov@jetbrains.com>
2023-03-24 16:11:22 +0100

    update: following project update

--- docs/snippets/multiplatform-mobile-tutorial/Entity.kt
+++ docs/snippets/multiplatform-mobile-tutorial/Entity.kt
@@ -1,5 +1,8 @@
 package com.jetbrains.handson.kmm.shared.entity
 
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toInstant
+import kotlinx.datetime.toLocalDateTime
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
@@ -7,36 +10,32 @@ import kotlinx.serialization.Serializable
 data class RocketLaunch(
     @SerialName("flight_number")
     val flightNumber: Int,
-    @SerialName("mission_name")
+    @SerialName("name")
     val missionName: String,
-    @SerialName("launch_year")
-    val launchYear: Int,
-    @SerialName("launch_date_utc")
+    @SerialName("date_utc")
     val launchDateUTC: String,
-    @SerialName("rocket")
-    val rocket: Rocket,
     @SerialName("details")
     val details: String?,
-    @SerialName("launch_success")
+    @SerialName("success")
     val launchSuccess: Boolean?,
     @SerialName("links")
     val links: Links
-)
-
-@Serializable
-data class Rocket(
-    @SerialName("rocket_id")
-    val id: String,
-    @SerialName("rocket_name")
-    val name: String,
-    @SerialName("rocket_type")
-    val type: String
-)
+) {
+    var launchYear = launchDateUTC.toInstant().toLocalDateTime(TimeZone.UTC).year
+}
 
 @Serializable
 data class Links(
-    @SerialName("mission_patch")
-    val missionPatchUrl: String?,
-    @SerialName("article_link")
-    val articleUrl: String?
+    @SerialName("patch")
+    val patch: Patch?,
+    @SerialName("article")
+    val article: String?
+)
+
+@Serializable
+data class Patch(
+    @SerialName("small")
+    val small: String?,
+    @SerialName("large")
+    val large: String?
 )
