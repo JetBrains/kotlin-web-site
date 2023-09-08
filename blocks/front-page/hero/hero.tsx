b37106a066176839e48db0d57240f89cc72d570c
Yuri Teplyakov <ronnie57x@gmail.com>
2023-09-08 10:44:44 +0200

    ktl-1116 feat: hero section

---
+++ blocks/front-page/hero/hero.tsx
@@ -0,0 +1,63 @@
+import React, { FC } from 'react';
+import Button from '@rescui/button';
+import cn from 'classnames';
+
+import { useTextStyles, createTextCn } from '@rescui/typography';
+import { ThemeProvider } from '@rescui/ui-contexts';
+
+import '@jetbrains/kotlin-web-site-ui/out/components/layout';
+
+import styles from './hero.module.css';
+
+import JBLogo from '../../../assets/jetbrains-logo.svg';
+import HeroImg from './images/hero-cover.png';
+import HeroImg2x from './images/hero-cover@2x.png';
+
+interface Props {}
+
+export const HeroSection: FC<Props> = () => {
+    const textCn = useTextStyles();
+    const darkTextCn = createTextCn('dark');
+
+    return (
+        <ThemeProvider theme={'dark'}>
+            <section className={styles.heroSection}>
+                <div className={cn('ktl-layout', 'ktl-layout--center')}>
+                    <div className={styles.grid}>
+                        <div className={styles.content}>
+                            <h1 className={cn(darkTextCn('rs-hero'), styles.heroText)}>
+                                Concise.
+                                <br /> Multiplatform.
+                                <br /> Fun.
+                            </h1>
+
+                            <img className={styles.imageMobile} src={HeroImg} srcSet={`${HeroImg2x} 2x`} alt="kotlin" />
+
+                            <div className={styles.info}>
+                                <Button size={'l'} href="/docs/getting-started.html">
+                                    Get Started
+                                </Button>
+                                <div className={styles.developer}>
+                                    <img src={JBLogo} alt="jetbrains logo" className={styles.developerLogo} />r{' '}
+                                    <div className={darkTextCn('rs-text-3', { hardness: 'hard' })}>
+                                        Kotlin, developed by&nbsp;
+                                        <a
+                                            href="https://www.jetbrains.com/"
+                                            className={darkTextCn('rs-link', { hardness: 'hard' })}
+                                            target={'_blank'}
+                                            rel={'noreferrer noopener'}
+                                        >
+                                            JetBrains
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <img className={styles.imageDesktop} src={HeroImg} srcSet={`${HeroImg2x} 2x`} alt="kotlin" />
+                    </div>
+                </div>
+            </section>
+        </ThemeProvider>
+    );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import GlobalHeader from '@jetbrains/kotlin-web-site-ui/out/components/header';
 import searchConfig from '../search-config.json';
 
+import { HeroSection } from '../blocks/front-page/hero/hero';
+
 function Index() {
     return (
         <>
             <GlobalHeader productWebUrl={''} hasSearch={true} searchConfig={searchConfig} darkHeader />
+            <HeroSection />
         </>
     );
 }
