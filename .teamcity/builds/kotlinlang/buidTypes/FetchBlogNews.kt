4a450baa124dd0eba4c4b0ac51ab2162edef30dc
zoobestik <kb.chernenko@gmail.com>
2023-12-07 22:15:17 +0100

    chore(ktl-1232): basic script

--- .teamcity/builds/kotlinlang/buidTypes/FetchBlogNews.kt
+++ .teamcity/builds/kotlinlang/buidTypes/FetchBlogNews.kt
@@ -4,4 +4,15 @@ import jetbrains.buildServer.configs.kotlin.BuildType
 
 object FetchBlogNews : BuildType({
     name = "Fetch Blog News"
+
+    vcs {
+        root(vcsRoots.KotlinLangOrg)
+    }
+
+    scripts {
+        script {
+            name = "fetch"
+            scriptContent = ""
+        }
+    }
 })
