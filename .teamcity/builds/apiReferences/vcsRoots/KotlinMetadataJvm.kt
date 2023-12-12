3fbdba56d11715debb6ef8d2fb3030ffd6b540a3
zoobestik <kb.chernenko@gmail.com>
2023-12-12 20:54:53 +0100

    fix; kotlin version for metadata

--- .teamcity/builds/apiReferences/vcsRoots/KotlinMetadataJvm.kt
+++ .teamcity/builds/apiReferences/vcsRoots/KotlinMetadataJvm.kt
@@ -5,10 +5,9 @@ import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
 object KotlinMetadataJvm : GitVcsRoot({
   name = "kotlin metadata vcs root"
   url = "git@github.com:JetBrains/kotlin.git"
-  branch = "v1.9.21"
+  branch = "refs/heads/v1.9.21"
   branchSpec = """
     +:refs/heads/(*)
-    +:(refs/merge/*)
   """.trimIndent()
   useTagsAsBranches = true
   checkoutPolicy = AgentCheckoutPolicy.USE_MIRRORS
