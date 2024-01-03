a04f87a7e25133cd3500feb946dd54bbea9fddbf
Nikolay Pachkov <nikolay.pachkov@jetbrains.com>
2024-01-03 18:37:51 +0100

    KTL-1314 fix: return old footer to Dokka templates

--- static/js/page/dokka-template/footer/index.jsx
+++ static/js/page/dokka-template/footer/index.jsx
@@ -1,12 +1,40 @@
 import React, { useEffect, useState } from "react";
-import GlobalFooter from '@jetbrains/kotlin-web-site-ui/out/components/footer';
+import GlobalFooter from '@jetbrains/kotlin-web-site-ui/out/components/footer-compact';
 import { ThemeProvider } from '@rescui/ui-contexts';
 import './index.scss';
 
 const Footer = () => {
+  const [theme, setTheme] = useState('light');
+
+  useEffect(() => {
+    if (typeof document !== `undefined`) {
+      const htmlEl = document.querySelector("html");
+      const isInitialThemeIsDark = htmlEl.classList.contains("theme-dark");
+
+      if (isInitialThemeIsDark) {
+        setTheme('dark');
+      }
+
+      function callback(mutationList) {
+        mutationList.forEach(function(mutation) {
+          if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
+            setTheme(htmlEl.classList.contains("theme-dark") ? 'dark' : 'light');
+          }
+        });
+      }
+
+      const observer = new MutationObserver(callback)
+      observer.observe(htmlEl, {
+        attributes: true
+      });
+
+      return observer.disconnect;
+    }
+  }, []);
+
   return (
-    <ThemeProvider theme={'dark'}>
-      <GlobalFooter className={'ktl-footer_dark'} />
+    <ThemeProvider theme={theme}>
+      <GlobalFooter className={theme === 'dark' ? 'ktl-footer_dark' : 'ktl-footer_light'} />
     </ThemeProvider>
   );
 }
