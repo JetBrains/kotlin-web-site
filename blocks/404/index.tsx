945317485563210567a94b340e6983e9e9c63868
Yuri Teplyakov <ronnie57x@gmail.com>
2024-01-30 10:42:09 +0100

    ktl-1244-404-page fix: style fixes

--- blocks/404/index.tsx
+++ blocks/404/index.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react';
+import cn from 'classnames';
 
 import Mascot404 from './404-mascot.svg';
 
@@ -12,8 +13,8 @@ export const NotFoundContent: FC = () => {
     return (
         <div className={styles.wrapper}>
             <img className={styles.image} src={Mascot404.src} alt="404"/>
-            <h1 className={textCn('rs-h1')}>Page not found</h1>
-            <p className={textCn('rs-text-2', { hardness: 'hard' })}>
+            <h1 className={cn(textCn('rs-h1'), styles.heading)}>Page not found</h1>
+            <p className={cn(textCn('rs-text-2', { hardness: 'hard' }), styles.subheading)}>
                 Please use search or try<br className={styles.lineBreak} /> starting from{' '}
                 <a className={textCn('rs-link', { hardness: 'hard', mode: 'rock' })} href="https://kotlinlang.org/">
                     home.
