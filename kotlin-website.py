658177164e55aa8f38677ac715e4662fb54321b0
berezinant <berezinant@gmail.com>
2024-02-06 22:10:10 +0100

    feat(KTL-1351): added route to local /docs pages in python

--- kotlin-website.py
+++ kotlin-website.py
@@ -236,6 +236,10 @@ def kotlin_reference_pdf():
 def kotlin_docs_pdf():
     return send_file(path.join(root_folder, "assets", "kotlin-reference.pdf"))
 
+@app.route('/docs/<path:path>')
+def docs(path):
+    return send_from_directory('dist/docs/', path)
+
 
 @app.route('/_next/<path:path>')
 def static_file(path):
