09145972f8cc3e118e52ec98aef2cf35516985c6
zoobestik <kb.chernenko@gmail.com>
2024-02-07 14:11:25 +0100

    chore; update Kotlin DSL version

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -22,7 +22,7 @@ To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 'Debug' option is available in the context menu for the task.
 */
 
-version = "2023.05"
+version = "2023.11"
 
 project {
   subProject(builds.kotlinlang.SiteProject)
