0bb3174bec5f6e790f723f897d9c8d4d72933be7
berezinant <berezinant@gmail.com>
2024-02-09 15:25:36 +0100

    test(KTL-1351): added full page test for /docs/test-page

--- test/visual/webhelp.spec.ts
+++ test/visual/webhelp.spec.ts
@@ -3,7 +3,15 @@ import { testSelector } from '../utils';
 import { WebHelpPage } from '../page/webhelp-page';
 
 test.describe('WebHelp page', async () => {
-    test(`Should render components properly on the /doc/test-page.html`, async ({ page }) => {
+    test(`Should render full article properly on /doc/test-page.html`, async ({ page }) => {
+        const webHelpPage = new WebHelpPage(page, '/docs/test-page.html');
+        await webHelpPage.init();
+
+        const screenshot = await page.screenshot({ fullPage: true });
+        expect(screenshot).toMatchSnapshot(`webhelp-test-full-page.png`);
+    });
+
+    test(`Should render micro format properly on the /doc/test-page.html`, async ({ page }) => {
         const webHelpPage = new WebHelpPage(page, '/docs/test-page.html');
         await webHelpPage.init();
 
