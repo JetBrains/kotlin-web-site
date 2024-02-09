ca653bcf2422ccbec6814fafe9f018119dd548e1
zoobestik <kb.chernenko@gmail.com>
2024-02-09 13:55:03 +0100

    chore(KTL-1003): stdlib reference links to 1.9.20

--- .teamcity/vcsRoots/KotlinLangOrg.kt
+++ .teamcity/vcsRoots/KotlinLangOrg.kt
@@ -6,7 +6,7 @@ object KotlinLangOrg: GitVcsRoot({
   name = "kotlinlang.org VCS root"
   url = "ssh://git@github.com/JetBrains/kotlin-web-site"
   branch = "refs/heads/master"
-  branchSpec = "+:refs/heads/*"
+  branchSpec = "+:refs/heads/(*)"
   authMethod = uploadedKey {
     uploadedKey = "default teamcity key"
   }
