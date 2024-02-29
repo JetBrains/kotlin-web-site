7fec7104494778ea7872ccb7df7b4867b62ed1cb
Dmitry Khalanskiy <52952525+dkhalanskyjb@users.noreply.github.com>
2024-02-29 15:50:25 +0300

    Update kotlinx-datetime to the most recent version

--- .teamcity/BuildParams.kt
+++ .teamcity/BuildParams.kt
@@ -3,7 +3,7 @@ object BuildParams {
 
   const val KOTLINX_COROUTINES_RELEASE_TAG = "1.8.0"
   const val KOTLINX_SERIALIZATION_RELEASE_TAG = "v1.6.2"
-  const val KOTLINX_DATETIME_RELEASE_TAG = "v0.5.0"
+  const val KOTLINX_DATETIME_RELEASE_TAG = "v0.6.0-RC"
   const val KOTLINX_METADATA_JVM_RELEASE_TAG = "v0.7.0"
 
   const val SEARCH_APP_ID = "7961PKYRXV"
