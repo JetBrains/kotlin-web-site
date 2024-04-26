0bc4955e5285cea9cc28c0f984683b05e4bc0c8c
zoobestik <kb.chernenko@gmail.com>
2024-04-26 18:01:25 +0200

    fix: add som required files for prepare dokka template

--- .teamcity/builds/apiReferences/templates/PrepareDokkaTemplate.kt
+++ .teamcity/builds/apiReferences/templates/PrepareDokkaTemplate.kt
@@ -10,6 +10,10 @@ object PrepareDokkaTemplate: Template({
   vcs {
     root(vcsRoots.KotlinLangOrg, """
       -:.
+      ?:.babelrc
+      ?:tsconfig.json
+      ?:search-config.json
+      
       +:package.json
       +:yarn.lock
       
