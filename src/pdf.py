963abde3b78be1d1d4a1c5e36a3b6b05b7dac458
zoobestik <kb.chernenko@gmail.com>
2024-05-29 18:47:38 +0200

    chore(pdf): fix broken external images

--- src/pdf.py
+++ src/pdf.py
@@ -67,6 +67,11 @@ def transform_book_content(path_file):
 
         soup = BeautifulSoup(html, "html.parser")
 
+        for node in soup.select('img[src^="images/https://"]'):
+            print('Fix image link "%s"' % node["src"])
+            node['src'] = node["src"][7:]
+            node['data-dark-src'] = node["data-dark-src"][7:]
+
         result_file = get_tmp_filename(path_file)
 
         with open(result_file, "w", encoding='utf-8') as output:
