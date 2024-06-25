78c53921036563beb0475a77a5faf7ddbc72a40e
zoobestik <kb.chernenko@gmail.com>
2024-06-25 18:05:01 +0200

    fix(teamcity): handle exceptions for node 18 by disabling experimental fetch

--- .teamcity/builds/kotlinlang/buidTypes/BuildJsAssets.kt
+++ .teamcity/builds/kotlinlang/buidTypes/BuildJsAssets.kt
@@ -26,6 +26,12 @@ object BuildJsAssets: BuildType({
         #!/bin/bash
         set -e -x -u
         
+        # for node:18 we caught exception:
+        #   unhandledRejection TypeError: Failed to parse URL from /mnt/agent/work/75f6fbaeb8c41e25/node_modules/@wasm-codecs/mozjpeg/lib/mozjpeg.wasm
+        #   at Object.fetch (node:internal/deps/undici/undici:11730:11) {
+        #   [cause]: TypeError: Invalid URL
+        export NODE_OPTIONS=--no-experimental-fetch
+        
         yarn install --frozen-lockfile
         
         NODE_ENV=production yarn run build
