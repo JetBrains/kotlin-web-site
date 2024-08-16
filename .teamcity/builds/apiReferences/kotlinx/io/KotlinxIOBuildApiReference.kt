8cc0a6656e2af9df0b0de34e787bc3af7db3b319
berezinant <berezinant@gmail.com>
2024-08-16 11:39:49 +0200

    fix(ktl-1576): run dokka for kotlinx-io

--- .teamcity/builds/apiReferences/kotlinx/io/KotlinxIOBuildApiReference.kt
+++ .teamcity/builds/apiReferences/kotlinx/io/KotlinxIOBuildApiReference.kt
@@ -4,15 +4,17 @@ import BuildParams.KOTLINX_IO_RELEASE_TAG
 import builds.apiReferences.dependsOnDokkaTemplate
 import builds.apiReferences.templates.BuildApiReference
 import builds.apiReferences.templates.buildDokkaHTML
+import builds.apiReferences.templates.scriptDokkaVersionSync
 import builds.apiReferences.templates.scriptDropSnapshot
 import jetbrains.buildServer.configs.kotlin.BuildType
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
 
 object KotlinxIOBuildApiReference : BuildType({
     name = "kotlinx-io API reference"
 
     templates(BuildApiReference)
 
-    artifactRules = "core/build/dokka/html/** => pages.zip"
+    artifactRules = "build/dokka/htmlMultiModule/** => pages.zip"
 
     params {
         param("release.tag", KOTLINX_IO_RELEASE_TAG)
@@ -28,13 +30,23 @@ object KotlinxIOBuildApiReference : BuildType({
 
     steps {
         scriptDropSnapshot {
-            scriptContent = """
-            #!/bin/bash
-            sed -i -E "s/versionSuffix=SNAPSHOT//gi" ./gradle.properties
-            """.trimIndent()
+          enabled = false // TODO readme, and disable this step for other libs too
+        }
+        scriptDokkaVersionSync {
+            enabled = false // TODO readme, and disable this step for other libs too
         }
         buildDokkaHTML {
-            tasks = ":kotlinx-io:dokkaHtmlMultiModule"
+            enabled = false // TODO add useGradlewWrapper = true
+        }
+        script {
+            name = "build api reference"
+            scriptContent = """
+                #!/bin/bash
+                
+                 set -e -u
+                
+                ./gradlew dokkaHtmlMultiModule --no-daemon --no-configuration-cache
+            """.trimIndent()
         }
     }
 })
