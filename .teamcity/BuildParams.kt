d8254bd0e920fd9eb0ae359fe76959842ba0e46d
nikpachoo <nikpachoo@gmail.com>
2024-11-18 15:32:59 +0100

    ktl-1399 fix: metadata version and naming

--- .teamcity/BuildParams.kt
+++ .teamcity/BuildParams.kt
@@ -5,7 +5,7 @@ object BuildParams {
   const val KOTLINX_SERIALIZATION_RELEASE_TAG = "v1.7.3"
   const val KOTLINX_DATETIME_RELEASE_TAG = "v0.6.1"
   const val KOTLINX_IO_RELEASE_TAG = "v0.5.4" // copy tag supported in master, remove "v" after next release 0.5.5 or 0.6.0
-  const val KOTLIN_RELEASE_TAG = "v2.0.20"
+  const val KOTLIN_RELEASE_TAG = "v2.0.21"
 
   const val SEARCH_APP_ID = "7961PKYRXV"
   const val SEARCH_INDEX_NAME = "prod_KOTLINLANG_WEBHELP"
