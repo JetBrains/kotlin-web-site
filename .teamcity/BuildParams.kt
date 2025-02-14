8d64fe182838a114a3c310550aa05200a5e6c833
zoobestik <kb.chernenko@gmail.com>
2025-02-14 15:52:25 +0100

    refactor(build-config): streamline release tag usage across build pipelines - Replace hardcoded tag prefixes with dynamic release labels - Update vcs branch specs to include both heads and tags for all configurations - Align naming consistency for release properties

--- .teamcity/BuildParams.kt
+++ .teamcity/BuildParams.kt
@@ -1,26 +1,29 @@
 object BuildParams {
-  const val DOKKA_TEMPLATES_VERSION = "1.9.10"
+  const val DOKKA_TEMPLATES_VERSION = "2.0.0"
 
   const val KOTLINX_COROUTINES_RELEASE_LABEL = "1.10.1"
-  const val KOTLINX_COROUTINES_RELEASE_TAG = "master"
+  const val KOTLINX_COROUTINES_RELEASE_TAG = "whyoleg/dokka2-sync" // master``
   const val KOTLINX_COROUTINES_ID = "kotlinx.coroutines"
 
   const val KOTLINX_SERIALIZATION_RELEASE_LABEL = "v1.8.1"
-  const val KOTLINX_SERIALIZATION_RELEASE_TAG = "master"
+  const val KOTLINX_SERIALIZATION_RELEASE_TAG = "whyoleg/dokka2-sync" // master
   const val KOTLINX_SERIALIZATION_ID = "kotlinx.serialization"
 
   const val KOTLINX_DATETIME_RELEASE_LABEL = "v0.6.2"
-  const val KOTLINX_DATETIME_RELEASE_TAG = "latest-release"
+  const val KOTLINX_DATETIME_RELEASE_TAG = "whyoleg/dokka2-sync" // latest-release
   const val KOTLINX_DATETIME_ID = "kotlinx-datetime"
 
-  const val KOTLINX_IO_RELEASE_TAG = "0.7.0"
+  const val KOTLINX_IO_RELEASE_LABEL = "0.7.0" // KOTLINX_IO_RELEASE_TAG
+  const val KOTLINX_IO_RELEASE_TAG = "whyoleg/dokka2-sync" // 0.6.0
   const val KOTLINX_IO_ID = "kotlinx-io"
 
-  const val KOTLINX_METADATA_ID = "kotlinx-metadata-jvm"
+  const val KOTLIN_RELEASE_LABEL = "2.1.0"
+  const val KOTLIN_RELEASE_TAG = "whyoleg/dokka2-sync-stdlib"
   const val KOTLIN_CORE_API_BUILD_ID = "Kotlin_KotlinRelease_2120_LibraryReferenceLatestDocs"
-  const val KOTLIN_RELEASE_TAG = "v2.1.0"
+  const val KOTLINX_METADATA_ID = "kotlinx-metadata-jvm"
 
-  val KGP_RELEASE_TAG = if (KOTLIN_RELEASE_TAG >= "v2.1.0") KOTLIN_RELEASE_TAG else "2.1.0"
+  const val KGP_RELEASE_LABEL = KOTLIN_RELEASE_LABEL
+  const val KGP_RELEASE_TAG = "whyoleg/dokka2-sync"
   const val KGP_ID = "kotlin-gradle-plugin"
 
   const val SEARCH_APP_ID = "7961PKYRXV"
