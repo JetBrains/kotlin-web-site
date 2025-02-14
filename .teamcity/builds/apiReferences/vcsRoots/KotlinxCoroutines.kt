a3a01f2612fbc1128232b7f48be5b561f3b943e0
zoobestik <kb.chernenko@gmail.com>
2025-02-14 14:42:33 +0100

    refactor(builds): update release tag variables to use labels dynamically

--- .teamcity/builds/apiReferences/vcsRoots/KotlinxCoroutines.kt
+++ .teamcity/builds/apiReferences/vcsRoots/KotlinxCoroutines.kt
@@ -1,11 +1,12 @@
 package builds.apiReferences.vcsRoots
 
+import BuildParams.KOTLINX_COROUTINES_RELEASE_TAG
 import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
 
 object KotlinxCoroutines : GitVcsRoot({
   name = "kotlinx.coroutines vcs root"
   url = "git@github.com:Kotlin/kotlinx.coroutines.git"
-  branch = "refs/heads/master"
+  branch = "refs/heads/$KOTLINX_COROUTINES_RELEASE_TAG"
   branchSpec = """
         +:refs/heads/(*)
     """.trimIndent()
--- .teamcity/builds/apiReferences/vcsRoots/KotlinxDatetime.kt
+++ .teamcity/builds/apiReferences/vcsRoots/KotlinxDatetime.kt
@@ -1,11 +1,12 @@
 package builds.apiReferences.vcsRoots
 
+import BuildParams.KOTLINX_DATETIME_RELEASE_TAG
 import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
 
 object KotlinxDatetime: GitVcsRoot({
   name = "kotlinx-datetime vcs root"
   url = "git@github.com:Kotlin/kotlinx-datetime.git"
-  branch = "refs/heads/latest-release"
+  branch = "refs/heads/$KOTLINX_DATETIME_RELEASE_TAG"
   branchSpec = """
         +:refs/heads/(*)
         +:refs/tags/(*)
