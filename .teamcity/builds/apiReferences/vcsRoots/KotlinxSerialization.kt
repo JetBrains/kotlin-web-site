27abd52958adfced90d6e1f322ff0357b4b95d73
zoobestik <kb.chernenko@gmail.com>
2025-02-14 15:52:25 +0100

    refactor(build-config): streamline release tag usage across build pipelines - Replace hardcoded tag prefixes with dynamic release labels - Update vcs branch specs to include both heads and tags for all configurations - Align naming consistency for release properties

--- .teamcity/builds/apiReferences/vcsRoots/KotlinxSerialization.kt
+++ .teamcity/builds/apiReferences/vcsRoots/KotlinxSerialization.kt
@@ -6,10 +6,11 @@ import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
 object KotlinxSerialization: GitVcsRoot({
   name = "kotlinx.serialization vcs root"
   url = "git@github.com:Kotlin/kotlinx.serialization.git"
-  branch = "refs/heads/$KOTLINX_SERIALIZATION_RELEASE_TAG"
+  branch = KOTLINX_SERIALIZATION_RELEASE_TAG
   branchSpec = """
-        +:refs/heads/(*)
-    """.trimIndent()
+    +:refs/heads/(*)
+    +:refs/tags/(*)
+  """.trimIndent()
   useTagsAsBranches = true
   authMethod = uploadedKey {
     uploadedKey = "teamcity"
