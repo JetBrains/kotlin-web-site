41875c15d2ca9a0b09708f009faa20052f0bf7f7
zoobestik <kb.chernenko@gmail.com>
2025-02-14 14:42:33 +0100

    refactor(builds): update release tag variables to use labels dynamically

--- .teamcity/builds/apiReferences/vcsRoots/KotlinxSerialization.kt
+++ .teamcity/builds/apiReferences/vcsRoots/KotlinxSerialization.kt
@@ -1,11 +1,12 @@
 package builds.apiReferences.vcsRoots
 
+import BuildParams.KOTLINX_SERIALIZATION_RELEASE_TAG
 import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
 
 object KotlinxSerialization: GitVcsRoot({
   name = "kotlinx.serialization vcs root"
   url = "git@github.com:Kotlin/kotlinx.serialization.git"
-  branch = "refs/heads/master"
+  branch = "refs/heads/$KOTLINX_SERIALIZATION_RELEASE_TAG"
   branchSpec = """
         +:refs/heads/(*)
     """.trimIndent()
