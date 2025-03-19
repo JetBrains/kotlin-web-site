ef3dbb1fe94766b21c0c97af17af8bfd83bc4788
zoobestik <kb.chernenko@gmail.com>
2025-03-19 16:55:38 +0100

    chore: KTL-2134; add KGP builds

--- .teamcity/BuildParams.kt
+++ .teamcity/BuildParams.kt
@@ -1,4 +1,6 @@
 import references.builds.compose.ComposeMultiplatformCore
+import references.builds.kgp.KotlinGradleAPI
+import common.extensions.VCS
 
 object BuildParams {
   const val DOKKA_TEMPLATES_VERSION = "2.0.0"
@@ -38,6 +40,11 @@ object BuildParams {
   const val KOTLINX_METADATA_RELEASE_TAG = KOTLIN_RELEASE_TAG
   const val KOTLINX_METADATA_TITLE = KOTLINX_METADATA_ID
 
+  val KGP_REFERENCE = KotlinGradleAPI {
+    addVersion("2.1.0", VCS.tag("v2.1.0"))
+    addVersion("2.1.20", VCS.branch("rrr/2.1.20/yahor/fix-api-reference-version-selector"))
+  }
+
   val API_COMPOSE = ComposeMultiplatformCore {
     addVersion("1.8.0", "api-reference/1.8")
   }
@@ -51,8 +58,8 @@ object BuildParams {
     "api/$KOTLINX_SERIALIZATION_ID",
     "api/$KOTLINX_DATETIME_ID",
     "api/$KOTLINX_IO_ID",
-    "api/$KGP_ID",
     "api/$KOTLINX_METADATA_ID",
+    "api/${KGP_REFERENCE.urlPart}",
     "api/${API_COMPOSE.urlPart}",
   )
 }
