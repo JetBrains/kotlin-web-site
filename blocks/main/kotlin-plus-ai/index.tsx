0c9a8e60949ef78d17fcc94d88946f39a4795bfe
zoobestik <kb.chernenko@gmail.com>
2025-05-21 16:50:34 +0200

    fixup! feat: add extra block on landing page

--- blocks/main/kotlin-plus-ai/index.tsx
+++ blocks/main/kotlin-plus-ai/index.tsx
@@ -60,7 +60,7 @@ function KotlinPlusAI() {
                             developing a simple chat assistant or an advanced multi-step workflow, Koog gives you full
                             control with clean Kotlin code – no external services are required. Build, extend, and
                             experiment with AI agents entirely in Kotlin.</p>
-                        <Button mode="rock" size={size} className={styles.getStarted} href="http://koog.ai/">Get started</Button>
+                        <Button mode="rock" size={size} className={styles.getStarted} href="https://docs.koog.ai/">Get started</Button>
                     </div>
                     <BlockScheme className={styles.buildAgentsScheme} />
                 </div>
