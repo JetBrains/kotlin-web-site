66211a789cddf05402ec8bd1b2c2eb8c185192e6
nikpachoo <Nikolay.Pachkov@jetbrains.com>
2025-07-11 00:20:54 +0200

    feat: server-side companies block

--- pages/server-side/index.tsx
+++ pages/server-side/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ServerSideLayout } from '../../blocks/server-side/layout/server-side-layout';
 import FeaturesSection from '../../blocks/server-side/features-section';
+import CustomerLogoMarqueeSection from '../../blocks/server-side/customer-logo-marquee';
 
 function Index() {
     return (
@@ -8,6 +9,7 @@ function Index() {
             <h1>Server side</h1>
 
             <FeaturesSection />
+            <CustomerLogoMarqueeSection />
         </ServerSideLayout>
     );
 }
