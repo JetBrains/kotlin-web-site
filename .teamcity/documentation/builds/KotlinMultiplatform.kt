e8ebe5dc580c7198f1d7bdabc195008c34032fb7
andrei.ezerskii <andrei.ezerskii@jetbrains.com>
2025-07-16 18:29:45 +0200

    KTL-2775 Update docker command to solve the issue with a new line

--- .teamcity/documentation/builds/KotlinMultiplatform.kt
+++ .teamcity/documentation/builds/KotlinMultiplatform.kt
@@ -25,14 +25,12 @@ object KotlinMultiplatform: BuildType ({
                 
                 docker run --rm -v %teamcity.build.checkoutDir%:/opt/sources \
                 registry.jetbrains.team/p/writerside/builder/writerside-builder:latest \
-                /bin/bash -c "
-                export DISPLAY=:99 && Xvfb :99 & /opt/builder/bin/idea.sh helpbuilderinspect \
+                /bin/bash -c "export DISPLAY=:99 && Xvfb :99 & /opt/builder/bin/idea.sh helpbuilderinspect \
                 --source-dir /opt/sources \
                 --product kotlin-multiplatform-docs/mpd \
                 --runner teamcity \
                 --frontend-url https://kotlinlang.org/docs/static/v3/ \
                 --output-dir /opt/sources/artifacts"
-                "  
             """.trimIndent()
         }
     }
