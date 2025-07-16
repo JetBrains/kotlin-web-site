41b50022284ef23da0b91a8babfd311fea6b7274
andrei.ezerskii <andrei.ezerskii@jetbrains.com>
2025-07-16 17:25:45 +0200

    Added new documentation package for stroring documentation builds on the kotlinlang side

---
+++ .teamcity/documentation/builds/KotlinWithCoroutines.kt
@@ -0,0 +1,8 @@
+package documentation.builds
+
+import jetbrains.buildServer.configs.kotlin.BuildType
+
+object KotlinWithCoroutines: BuildType ({
+    name = "Kotlin Reference with coroutines"
+    description = "Kotlin Reference with coroutines documentation build"
+})
