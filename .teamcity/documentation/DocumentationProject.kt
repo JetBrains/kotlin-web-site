30eddce3566c580137b934ea3d11edd0ea4159c7
andrei.ezerskii <andrei.ezerskii@jetbrains.com>
2025-07-16 17:25:45 +0200

    Added new documentation package for stroring documentation builds on the kotlinlang side

---
+++ .teamcity/documentation/DocumentationProject.kt
@@ -0,0 +1,19 @@
+package documentation
+
+import documentation.builds.KotlinMultiplatform
+import documentation.builds.KotlinWithCoroutines
+import jetbrains.buildServer.configs.kotlin.Project
+
+object DocumentationProject: Project({
+    name = "Documentation"
+    description = "Build kotlin documentation"
+
+    buildTypesOrder = arrayListOf(
+        KotlinWithCoroutines,
+        KotlinMultiplatform,
+    )
+
+    buildTypesOrder.forEach {
+        buildType(it)
+    }
+})
---
+++ .teamcity/documentation/builds/KotlinMultiplatform.kt
@@ -0,0 +1,8 @@
+package documentation.builds
+
+import jetbrains.buildServer.configs.kotlin.BuildType
+
+object KotlinMultiplatform: BuildType ({
+    name = "Kotlin Multiplatform"
+    description = "Kotlin Multiplatform documentation build"
+})
