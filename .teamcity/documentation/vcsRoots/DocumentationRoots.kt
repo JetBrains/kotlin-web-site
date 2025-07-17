e026da5242133d9e82adddda1b85aced03287259
andrei.ezerskii <andrei.ezerskii@jetbrains.com>
2025-07-17 08:50:09 +0200

    KTL-2775 added build for the kotlin documentation

--- .teamcity/documentation/vcsRoots/DocumentationRoots.kt
+++ .teamcity/documentation/vcsRoots/DocumentationRoots.kt
@@ -2,9 +2,67 @@ package documentation.vcsRoots
 
 import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
 
+
+// KmpRoots
 object KotlinMultiplatformVCS: GitVcsRoot ({
     name = "Kotlin Multiplatform root"
     url = "https://github.com/JetBrains/kotlin-multiplatform-dev-docs.git"
+    branch = "refs/heads/master"
+    branchSpec = "+:refs/heads/*"
+    checkoutPolicy = AgentCheckoutPolicy.AUTO
+    authMethod = uploadedKey {
+        uploadedKey = "default teamcity key"
+    }
+})
+
+// Kotlin Reference With Coroutine Roots
+object KotlinReferenceRoot: GitVcsRoot ({
+    name = "Kotlin Reference"
+    url = "https://github.com/JetBrains/kotlin-web-site.git"
+    branch = "refs/heads/master"
+    branchSpec = "+:refs/heads/*"
+    checkoutPolicy = AgentCheckoutPolicy.AUTO
+    authMethod = uploadedKey {
+        uploadedKey = "default teamcity key"
+    }
+})
+
+object KotlinxCoroutinesRoot: GitVcsRoot ({
+    name = "Kotlinx Coroutines"
+    url = "https://github.com/Kotlin/kotlinx.coroutines.git"
+    branch = "refs/heads/master"
+    branchSpec = "+:refs/heads/*"
+    checkoutPolicy = AgentCheckoutPolicy.AUTO
+    authMethod = uploadedKey {
+        uploadedKey = "default teamcity key"
+    }
+})
+
+object KotlinxLincheckRoot: GitVcsRoot ({
+    name = "Kotlinx Lincheck"
+    url = "https://github.com/JetBrains/lincheck.git"
+    branch = "refs/heads/master"
+    branchSpec = "+:refs/heads/*"
+    checkoutPolicy = AgentCheckoutPolicy.AUTO
+    authMethod = uploadedKey {
+        uploadedKey = "default teamcity key"
+    }
+})
+
+object DokkaRoot: GitVcsRoot ({
+    name = "Dokka"
+    url = "https://github.com/Kotlin/dokka.git"
+    branch = "refs/heads/master"
+    branchSpec = "+:refs/heads/*"
+    checkoutPolicy = AgentCheckoutPolicy.AUTO
+    authMethod = uploadedKey {
+        uploadedKey = "default teamcity key"
+    }
+})
+
+object APIGuidelinesRoot: GitVcsRoot ({
+    name = "API Guidelines"
+    url = "https://github.com/Kotlin/api-guidelines.git"
     branch = "refs/heads/master"
     branchSpec = "+:refs/heads/*"
     checkoutPolicy = AgentCheckoutPolicy.AUTO
