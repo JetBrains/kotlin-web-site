2cfa5f9b785dbe1c8fe4d95791cba4aed5619568
andrei.ezerskii <andrei.ezerskii@jetbrains.com>
2025-07-18 11:05:12 +0200

    KTL-2775 Replace docker build step for the KMP documentation (use environment variables)

--- .teamcity/documentation/builds/KotlinMultiplatform.kt
+++ .teamcity/documentation/builds/KotlinMultiplatform.kt
@@ -5,6 +5,8 @@ import jetbrains.buildServer.configs.kotlin.BuildType
 import jetbrains.buildServer.configs.kotlin.buildSteps.script
 
 object KotlinMultiplatform: BuildType ({
+    val dockerImageTag = "2.1.2176-p8483"
+
     name = "Kotlin Multiplatform"
     description = "KTL-2775 Migrate KMP Dev Docs: Create a Build of KMP Dev Docs in Our Pipeline"
 
@@ -17,20 +19,32 @@ object KotlinMultiplatform: BuildType ({
     """.trimIndent()
 
     steps {
+//        script {
+//            name = "Build documentation"
+//            scriptContent = """
+//                #!/bin/bash
+//                set -e -x -u
+//
+//                docker run --rm -v %teamcity.build.checkoutDir%:/opt/sources \
+//                registry.jetbrains.team/p/writerside/builder/writerside-builder:latest \
+//                /bin/bash -c "export DISPLAY=:99 && Xvfb :99 & /opt/builder/bin/idea.sh helpbuilderinspect \
+//                --source-dir /opt/sources \
+//                --product kotlin-multiplatform-docs/mpd \
+//                --runner teamcity \
+//                --frontend-url https://kotlinlang.org/docs/static/v3/ \
+//                --output-dir /opt/sources/artifacts"
+//            """.trimIndent()
+//        }
         script {
-            name = "Build documentation"
-            scriptContent = """
-                #!/bin/bash
-                set -e -x -u
-                
-                docker run --rm -v %teamcity.build.checkoutDir%:/opt/sources \
-                registry.jetbrains.team/p/writerside/builder/writerside-builder:latest \
-                /bin/bash -c "export DISPLAY=:99 && Xvfb :99 & /opt/builder/bin/idea.sh helpbuilderinspect \
-                --source-dir /opt/sources \
-                --product kotlin-multiplatform-docs/mpd \
-                --runner teamcity \
-                --frontend-url https://kotlinlang.org/docs/static/v3/ \
-                --output-dir /opt/sources/artifacts"
+            name = "Build KMP Documentation with the docker"
+            dockerImage = "registry.jetbrains.team/p/writerside/builder/writerside-builder:$dockerImageTag"
+            dockerRunParameters = """
+                --rm -v %teamcity.build.checkoutDir%:/opt/sources \
+                -e SOURCE_DIR=/opt/sources \
+                -e PRODUCT=kotlin-multiplatform-docs/mpd \
+                -e RUNNER=teamcity \
+                -e FRONTEND=https://kotlinlang.org/docs/static/v3/ \
+                -e OUTPUT_DIR=/opt/sources/artifacts
             """.trimIndent()
         }
     }
