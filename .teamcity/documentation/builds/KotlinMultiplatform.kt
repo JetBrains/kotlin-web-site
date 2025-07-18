86814af683d908b691bf0f3458dc5378d439a3e9
andrei.ezerskii <andrei.ezerskii@jetbrains.com>
2025-07-18 12:57:09 +0200

    KTL-2775 update build name for KMP

--- .teamcity/documentation/builds/KotlinMultiplatform.kt
+++ .teamcity/documentation/builds/KotlinMultiplatform.kt
@@ -43,6 +43,7 @@ object KotlinMultiplatform: WritersideBuilder (
     module = "kotlin-multiplatform-docs",
     instance = "mpd",
     customInit = {
+        name = "Kotlin Multiplatform Documentation"
         vcs {
             root(KotlinMultiplatformVCS)
             cleanCheckout = true
