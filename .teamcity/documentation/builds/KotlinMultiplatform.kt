246539825a262536a44a22d108deecd58a1fc265
zoobestik <kb.chernenko@gmail.com>
2025-10-02 15:05:57 +0200

    chore(docs): add search configuration step in KotlinMultiplatform build

--- .teamcity/documentation/builds/KotlinMultiplatform.kt
+++ .teamcity/documentation/builds/KotlinMultiplatform.kt
@@ -2,6 +2,8 @@ package documentation.builds
 
 import documentation.vcsRoots.KotlinMultiplatformVCS
 import jetbrains.buildServer.configs.kotlin.CheckoutMode
+import jetbrains.buildServer.configs.kotlin.buildSteps.ScriptBuildStep
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
 
 object KotlinMultiplatform: WritersideBuilder (
     module = "kotlin-multiplatform-docs",
@@ -12,6 +14,29 @@ object KotlinMultiplatform: WritersideBuilder (
             root(KotlinMultiplatformVCS)
             cleanCheckout = true
             checkoutMode = CheckoutMode.ON_AGENT
+        }
+
+        steps {
+            script {
+                name = "Add search productId"
+
+                workingDir = "artifacts"
+                //language=sh
+                scriptContent = """
+                    #!/bin/sh
+                    unzip -p webHelpMPD2.zip config.json > config.json
+                    
+                    cp -f config.json temp.json
+                    sed '1s/"productId":/"searchProductId":"help\/kotlin-reference","productId":/' temp.json > config.json
+                    rm temp.json
+                    
+                    apk add zip
+                    zip -u webHelpMPD2.zip config.json
+                """.trimIndent()
+
+                dockerImagePlatform = ScriptBuildStep.ImagePlatform.Linux
+                dockerImage = "alpine:latest"
+            }
         }
     }
 )
