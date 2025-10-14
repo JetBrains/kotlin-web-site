8f31d278695d97951416e86c0e455c8af958f7d0
berezinant <berezinant@gmail.com>
2025-10-14 12:12:52 +0200

    fix(ktl-2781): proper og image and tile and description

--- pages/case-studies/index.tsx
+++ pages/case-studies/index.tsx
@@ -6,7 +6,11 @@ import { CaseStudiesGrid } from '../../blocks/case-studies/grid/case-studies-gri
 
 function Index() {
     return (
-        <ServerSideLayout title={'Case Studies'} ogImageName={'case-studies.png'}>
+        <ServerSideLayout
+            title="Kotlin Case Studies | Mobile, Web & Server-side"
+            ogImageName={'case-studies.png'}
+            description="Discover how teams use Kotlin to build backend services, multiplatform apps, and modern UIs. Explore real-world case studies and success stories."
+        >
             <CaseStudiesHero />
             <CaseStudiesFilter />
             <CaseStudiesGrid />
