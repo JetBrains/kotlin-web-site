65636f8b378878b87d63e9d09af5c5a3ab3a7782
berezinant <berezinant@gmail.com>
2025-11-10 15:03:03 +0100

    fix(ktl-2781): added empty state message just in case

--- blocks/case-studies/grid/case-studies-grid.tsx
+++ blocks/case-studies/grid/case-studies-grid.tsx
@@ -2,6 +2,7 @@ import { CaseStudyCard } from '../card/case-studies-card';
 import styles from './case-studies-grid.module.css';
 import { useFilteredCases } from '../filter/use-filtered-cases';
 import { MasonryGrid } from '../../../components/masonry-grid/masonry-grid';
+import { EmptyState } from '../../../components/empty-state/empry-state';
 
 export const CaseStudiesGrid: React.FC = () => {
     const cases = useFilteredCases();
@@ -10,13 +11,15 @@ export const CaseStudiesGrid: React.FC = () => {
         <section className="ktl-layout ktl-layout--center" data-testid="case-studies-grid"
                  aria-label="Case Studies Grid">
             <div className={styles.wrapper}>
-                <MasonryGrid
-                    items={cases}
-                    columnCount={2}
-                    gap={32}
-                    renderItem={(caseItem) => <CaseStudyCard {...caseItem} />}
-                    getKey={(caseItem) => caseItem.id}
-                />
+                {cases.length === 0 ?
+                    <EmptyState /> :
+                    <MasonryGrid
+                        items={cases}
+                        columnCount={2}
+                        gap={32}
+                        renderItem={(caseItem) => <CaseStudyCard {...caseItem} />}
+                        getKey={(caseItem) => caseItem.id}
+                    />}
             </div>
         </section>
     );
