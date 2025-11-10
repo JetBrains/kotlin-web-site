837b6819fb5d358771e6b4611508ab0964a040b8
berezinant <berezinant@gmail.com>
2025-11-10 16:29:53 +0100

    fix(ktl-2781): update content

--- blocks/case-studies/hero/case-studies-hero.tsx
+++ blocks/case-studies/hero/case-studies-hero.tsx
@@ -6,17 +6,17 @@ import styles from './case-studies-hero.module.css';
 export const CaseStudiesHero: React.FC = () => {
     const textCn = useTextStyles();
     return (
-        <section className="ktl-layout ktl-layout--center" data-testid="case-studies-hero" aria-label="Case Studies Hero">
+        <section className="ktl-layout ktl-layout--center" data-testid="case-studies-hero"
+                 aria-label="Case Studies Hero">
             <div className={styles.content}>
                 <h1 className={cn(textCn('rs-hero'), styles.title)} data-testid="case-studies-hero-title">
                     Case studies
                 </h1>
                 <p className={cn(textCn('rs-text-1'), styles.subtitle)} data-testid="case-studies-hero-subtitle">
                     Discover how companies of all sizes and industries apply Kotlin across client and server. From
-                    building cross-platform applications with shared logic and UI using Kotlin Multiplatform and
-                    Compose Multiplatform to developing reliable, high-performance backends with Kotlin — explore
-                    real-world strategies, integration approaches, and the benefits teams gain from using Kotlin in
-                    production.
+                    building cross-platform applications with shared logic and UI to developing reliable,
+                    high-performance backends — explore real-world strategies, integration approaches, and the benefits
+                    teams gain from using Kotlin in production.
                 </p>
             </div>
         </section>
