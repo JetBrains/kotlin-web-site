a8fa044316ec11145f453d4baf45173cba386ee5
andrei.ezerskii <andrei.ezerskii@jetbrains.com>
2025-11-11 14:17:28 +0100

    Update WritersideBuilder to new docker image version and add timestamps generation

--- .teamcity/documentation/builds/WritersideBuilder.kt
+++ .teamcity/documentation/builds/WritersideBuilder.kt
@@ -9,7 +9,7 @@ abstract class WritersideBuilder(
     instance: String,
     customInit: BuildType.() -> Unit = {}
 ): BuildType({
-    val dockerImageTag = "2025.07.8502"
+    val dockerImageTag = "2.1.2180-p8506"
     val frontend = "file:///opt/static/"
 
     name = "${instance.uppercase()} documentation build"
@@ -17,9 +17,20 @@ abstract class WritersideBuilder(
 
     artifactRules = """
         artifacts/*
+        timestamps.json
     """.trimIndent()
 
     steps {
+        script {
+            name = "Generate timestamps file"
+            scriptContent = """
+                echo "Generate timestamps"
+                echo "{" > %teamcity.build.checkoutDir%/timestamps.json
+                git ls-tree -r --name-only HEAD | grep -E '\.(topic|md)$' | xargs -n 1 -P 4 -I{} bash -c 'echo -e "\"$0\": \"$(git log -1 --format="%at" -- "$0")\","' {} >> %teamcity.build.checkoutDir%/timestamps.json
+                sed -i '$ s/.$//' %teamcity.build.checkoutDir%/timestamps.json
+                echo "}" >> %teamcity.build.checkoutDir%/timestamps.json
+            """.trimIndent()
+        }
         script {
             name = "Build $module/$instance documentation with the docker"
             scriptContent = """
@@ -35,6 +46,7 @@ abstract class WritersideBuilder(
                 -e RUNNER=teamcity
                 -e FRONTEND=$frontend
                 -e OUTPUT_DIR=/opt/sources/artifacts
+                -e TIME=/opt/sources/timestamps.json
             """.trimIndent()
         }
     }
