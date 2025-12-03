5ed07621be14c57f7fe5769b06c7ab69e2f354a7
nikpachoo <Nikolay.Pachkov@jetbrains.com>
2025-12-03 19:39:26 +0100

    ktl-3872 feat: KMP landing video blocks

--- pages/multiplatform/index.tsx
+++ pages/multiplatform/index.tsx
@@ -5,6 +5,9 @@ import { LandingLayout, LandingLayoutProps } from '../../components/landing-layo
 
 import { FaqBlock } from '../../blocks/multiplatform/faq-block/faq-block';
 import { HeroBanner } from '../../blocks/multiplatform/hero';
+import { VideoBlock } from '../../blocks/multiplatform/video-block/video-block';
+import { useTextStyles } from '@rescui/typography';
+import { HighlightedBg } from '../../blocks/multiplatform/highlighted-bg/highlighted-bg';
 import { CtaBlock } from '../../blocks/multiplatform/cta-block/cta-block';
 
 const MULTIPLATFORM_MOBILE_TITLE = 'Kotlin Multiplatform' as const;
@@ -24,12 +27,17 @@ const TOP_MENU_ITEMS: LandingLayoutProps['topMenuItems'] = [
 const GET_STARTED_URL = '/docs/multiplatform/get-started.html' as const;
 
 export default function MultiplatformLanding() {
+    const textCn = useTextStyles();
+    const linkClass = textCn('rs-link');
+
     return (
         <LandingLayout
             dataTestId={'multiplatform-landing'}
             title={'Kotlin Multiplatform – Build Cross-Platform Apps'}
             ogImageName={'multiplatform.png'}
-            description={'Kotlin Multiplatform is a technology for reusing up to 100% of your code across Android, iOS, web, and desktop, with Compose Multiplatform for shared UIs.'}
+            description={
+                'Kotlin Multiplatform is a technology for reusing up to 100% of your code across Android, iOS, web, and desktop, with Compose Multiplatform for shared UIs.'
+            }
             currentTitle={MULTIPLATFORM_MOBILE_TITLE}
             currentUrl={MULTIPLATFORM_MOBILE_URL}
             topMenuTitle={MULTIPLATFORM_MOBILE_TITLE}
@@ -41,6 +49,36 @@ export default function MultiplatformLanding() {
             <div className="ktl-layout-to-2">
                 <ThemeProvider theme={'dark'}>
                     <HeroBanner url={GET_STARTED_URL} />
+
+                    <HighlightedBg>
+                        <VideoBlock
+                            title={'AI-powered code generation'}
+                            videoId={'JuP1QawqZtI'}
+                            iconPath={'/images/multiplatform/video-block/junie.svg'}
+                            textWidthLimit={'917px'}
+                        >
+                            <>
+                                Speed up your multiplatform coding with code generation powered by{' '}
+                                <a className={linkClass} href={'https://www.jetbrains.com/junie/'}>Junie</a>, the coding agent by JetBrains.
+                            </>
+                        </VideoBlock>
+
+                        <VideoBlock
+                            title={'Seamless tooling'}
+                            videoId={'ACmerPEQAWA'}
+                            iconPath={'/images/multiplatform/video-block/infinity.svg'}
+                            textWidthLimit={'993px'}
+                        >
+                            <>
+                                Enjoy smart IDE support with the{' '}
+                                <a className={linkClass} href={'https://plugins.jetbrains.com/plugin/14936-kotlin-multiplatform'}>KMP plugin</a>{' '}for{' '}
+                                <a className={linkClass} href={'https://www.jetbrains.com/idea/download/'}>IntelliJ IDEA</a>{' '}and{' '}
+                                <a className={linkClass} href={'https://developer.android.com/studio'}>Android Studio</a>, with common previews,
+                                cross-language navigation, refactorings, and debugging across Kotlin and Swift code.
+                            </>
+                        </VideoBlock>
+                    </HighlightedBg>
+
                     <FaqBlock />
                     <CtaBlock url={GET_STARTED_URL} />
                 </ThemeProvider>
