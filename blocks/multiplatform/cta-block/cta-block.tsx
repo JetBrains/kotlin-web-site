1080c3886bc1870ef0210526ae6c8380ec24e2c9
berezinant <berezinant@gmail.com>
2025-12-05 10:46:01 +0100

    feat(ktl-3875): added cta block for kmp landing

---
+++ blocks/multiplatform/cta-block/cta-block.tsx
@@ -0,0 +1,27 @@
+import { useDL } from '@jetbrains/kotlin-web-site-ui/out/components/breakpoints-v2';
+import { Button } from '@rescui/button';
+import { useTextStyles } from '@rescui/typography';
+import cn from 'classnames';
+import Img from 'next/image';
+import ctaGraphics from './images/cta-graphics.webp';
+import styles from './cta-block.module.css';
+
+export const CtaBlock = ({ url }: { url: string }) => {
+    const isDL = useDL();
+    const textCn = useTextStyles();
+    return (
+        <div className={styles.wrapper}>
+            <div className="ktl-layout ktl-layout--center">
+                <div className={styles.content}>
+                <h2 className={cn(textCn('rs-h1'), styles.title)}>
+                    Try Kotlin Multiplatform&nbsp;—<br />share code on your terms across different platforms.
+                </h2>
+                <Button className={cn(styles.button)} mode={'rock'} href={url} size={isDL ? 'l' : 'm'}>
+                    Get Started
+                </Button>
+                    <Img src={ctaGraphics} alt="" className={styles.image} height={450} />
+                </div>
+            </div>
+        </div>
+    );
+};
