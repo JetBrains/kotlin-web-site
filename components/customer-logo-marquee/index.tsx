7cdc12463c7f97582d465a6953f0a1efa7684e63
zoobestik <kb.chernenko@gmail.com>
2025-12-08 19:21:55 +0100

    feat(components): reorganize customer logo data for cleanup

--- components/customer-logo-marquee/index.tsx
+++ components/customer-logo-marquee/index.tsx
@@ -3,7 +3,8 @@ import { Marquee } from '../marquee';
 import styles from './index.module.css';
 
 export interface LogoItem {
-    id: ImgSrc;
+    id: string;
+    logo: string;
     link: string;
 }
 
@@ -11,16 +12,16 @@ const CustomerLogoMarqueeSection: React.FC<{ items: LogoItem[] }> = ({ items }) 
     return (
         <section className={styles.section} data-testid={'customers-block'}>
             <Marquee className={styles.list} pauseOnHover hasFadingEdges>
-                {items.map((item, index) => (
+                {items.map((item) => (
                     <a
+                        key={item.id}
                         href={item.link}
-                        key={`logo_${index}`}
                         target="_blank"
                         rel="noopener noreferrer"
                         className={styles.logoLink}
                         data-testid="customers-link"
                     >
-                        <img src={item.id.src} alt={item.link} className={styles.logo} />
+                        <img src={item.logo} alt={item.link} className={styles.logo} />
                     </a>
                 ))}
             </Marquee>
