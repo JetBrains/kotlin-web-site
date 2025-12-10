1324bfad3470f71d9519f2ec9b97a2aa63440894
zoobestik <kb.chernenko@gmail.com>
2025-12-10 15:21:05 +0100

    fix(multiplatform): second video is cropped
    - Refactor video-related files into `videos` directory for better modularity.
    - Introduce `CoolVideos` component to simplify video block usage in the Multiplatform landing page.
    - Replace inline video declarations with reusable `CoolVideos` component.
    - Enhance `VideoBlock` to support additional `MiniVideoPlayerProps` and custom class names.
    - Add CSS variables for video player styling, including aspect ratio and border radius.
    - Improve reusability and maintainability by applying semantic naming and resolving duplicate code.
    
    fix(css): update video player border radius to match design guidelines
    
    > It seems like the videos would look better with a smaller corner radius - let's go with 16px, like the cards below.
    
    https://www.figma.com/design/zctILurzM32OmUgLay5RpZ/KMP-landing-page-%7C-Kotlinlang-%7C-DSGN-24366

---
+++ blocks/multiplatform/videos/videos.tsx
@@ -0,0 +1,40 @@
+import { useTextStyles } from '@rescui/typography';
+import { HighlightedBg } from '../highlighted-bg/highlighted-bg';
+import { VideoBlock } from './video-block/video-block';
+
+import styles from './videos.module.css';
+
+export function CoolVideos() {
+    const textCn = useTextStyles();
+    const linkClass = textCn('rs-link');
+
+    return <HighlightedBg>
+        <VideoBlock
+            title={'AI-powered code generation'}
+            source={'https://resources.jetbrains.com/storage/products/kotlin/landings/kmp_and_junie.mp4'}
+            iconPath={'/images/multiplatform/video-block/junie.svg'}
+            textWidthLimit={'917px'}
+        >
+            Speed up your multiplatform coding with code generation powered by{' '}
+            <a className={linkClass} href={'https://www.jetbrains.com/junie/'}>Junie</a>, the coding agent by
+            JetBrains.
+        </VideoBlock>
+
+        <VideoBlock
+            className={styles.composeHotReload}
+            title={'Seamless tooling'}
+            source={'https://resources.jetbrains.com/storage/products/kotlin/landings/kmp_compose_hot_reload.mp4'}
+            iconPath={'/images/multiplatform/video-block/infinity.svg'}
+            textWidthLimit={'993px'}
+        >
+            Enjoy smart IDE support with the{' '}
+            <a className={linkClass} href={'https://plugins.jetbrains.com/plugin/14936-kotlin-multiplatform'}>KMP
+                plugin</a>{' '}for{' '}
+            <a className={linkClass} href={'https://www.jetbrains.com/idea/download/'}>IntelliJ
+                IDEA</a>{' '}and{' '}
+            <a className={linkClass} href={'https://developer.android.com/studio'}>Android Studio</a>, with common
+            previews,
+            cross-language navigation, refactorings, and debugging across Kotlin and Swift code.
+        </VideoBlock>
+    </HighlightedBg>;
+}
