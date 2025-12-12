39e057560836ca90334d04fb544a5d8c3a18bec2
zoobestik <kb.chernenko@gmail.com>
2025-12-12 18:30:51 +0100

    test(multiplatform): update "Choose what to share" and CTA block tests
    - Refactor and enhance test logic for chip actions and tab validation.
    - Add `checkChooseWhatToShare` utility for reusability.
    - Update tests to validate proper click behavior and content visibility.
    - Simplify `MultiplatformPage` locators with consolidated `shareWhatChips` field.

--- test/e2e/multiplatform.spec.ts
+++ test/e2e/multiplatform.spec.ts
@@ -1,4 +1,4 @@
-import { expect, test } from '@playwright/test';
+import { expect, Locator, Page, test } from '@playwright/test';
 import { MultiplatformPage } from '../page/multiplatform-page';
 import { checkAnchor } from '../utils';
 
@@ -32,36 +32,34 @@ test.describe('Multiplatform landing page', async () => {
         await expect(multiplatformPage.shareWhatBlock).toBeVisible();
         await expect(multiplatformPage.shareWhatTitle).not.toBeEmpty();
 
-        const anchors = multiplatformPage.shareWhatChipAnchor;
-        const anchorCount = await anchors.count();
+        const anchors = multiplatformPage.shareWhatChips;
+        await expect(anchors).toHaveCount(3);
 
-        for (let i = 0; i < anchorCount; i++) {
-            await multiplatformPage.shareWhatBlock.scrollIntoViewIfNeeded();
-            const anchor = multiplatformPage.shareWhatChipAnchor.nth(i);
+        for (const anchor of await anchors.all()) {
+            // Check that the "both-logic-ui-tab" chip is selected by default
+            if (await anchor.getAttribute('id') === 'choose-share-what-both-logic-ui-tab') {
+                await anchor.scrollIntoViewIfNeeded();
+                await checkChooseWhatToShare(page, anchor);
+                continue;
+            }
+
+            // Check that other chips are not selected by default
+            await expect(anchor).not.toHaveAttribute('aria-selected', 'true');
+            await expect(anchor).not.toHaveAttribute('data-test', /(^|\s)chip-selected(\s|$)/);
+
+            const paneId = await anchor.getAttribute('aria-controls');
+            const pane = page.locator(`#${paneId}`);
+
+            // Check that the corresponding pane is not visible by default
+            await expect(pane).not.toBeVisible();
+        }
+
+        // Check that clicking on a chip selects it and opens the corresponding pane
+        //   reversed for test it works for all tabs includes the first one
+        for (const anchor of (await anchors.all()).reverse()) {
             await anchor.scrollIntoViewIfNeeded();
             await checkAnchor(page, anchor);
-
-            const title = multiplatformPage.shareWhatChipContentTitle.nth(i);
-            await expect(title).toBeVisible();
-            await expect(title).not.toBeEmpty();
-
-            const content = multiplatformPage.shareWhatChipContent.nth(i);
-            await expect(content).toBeVisible();
-            await expect(content).not.toBeEmpty();
-
-            const link = multiplatformPage.shareWhatChipContentAction.nth(i);
-            await expect(link).toBeVisible();
-            await expect(link).not.toBeEmpty();
-
-            const href = await link.getAttribute('href');
-            await link.click();
-
-            expect(page.url()).toContain(href);
-            await page.goBack();
-
-            const relink = multiplatformPage.shareWhatChipContentAction.nth(i);
-            await expect(relink).toBeVisible();
-            await expect(relink).not.toBeEmpty();
+            await checkChooseWhatToShare(page, anchor);
         }
     });
 
@@ -78,3 +76,37 @@ test.describe('Multiplatform landing page', async () => {
         expect(page.url()).toContain(href);
     });
 });
+
+async function checkChooseWhatToShare(page: Page, anchor: Locator) {
+    // Check that the tab is selected
+    await expect(anchor).toHaveAttribute('aria-selected', 'true');
+    await expect(anchor).toHaveAttribute('data-test', /(^|\s)chip-selected(\s|$)/);
+
+    const paneId = await anchor.getAttribute('aria-controls');
+    const pane = page.locator(`#${paneId}`);
+
+    await expect(pane).toBeInViewport();
+
+    // Check that the selected chip content is visible and not empty
+    const title = pane.getByTestId('share-what-chip-content-title');
+    await expect(title).toBeVisible();
+    await expect(title).not.toBeEmpty();
+
+    const content = pane.getByTestId('share-what-chip-content-text');
+    await expect(content).toBeVisible();
+    await expect(content).not.toBeEmpty();
+
+    const link = pane.getByTestId('share-what-chip-content-action');
+    await expect(link).toBeVisible();
+    await expect(link).not.toBeEmpty();
+
+    // Check that links work as links
+    const href = await link.getAttribute('href');
+    await link.click();
+
+    expect(page.url()).toContain(href);
+    await page.goBack();
+
+    // Check that the browser scrolls to the anchor after clicking on the back button
+    await expect(link).toBeInViewport();
+}
