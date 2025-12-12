d507f37ac729bb6f96a0a2e03ca35d577ed1c176
zoobestik <kb.chernenko@gmail.com>
2025-12-12 18:30:51 +0100

    test(multiplatform): update "Choose what to share" and CTA block tests
    - Refactor and enhance test logic for chip actions and tab validation.
    - Add `checkChooseWhatToShare` utility for reusability.
    - Update tests to validate proper click behavior and content visibility.
    - Simplify `MultiplatformPage` locators with consolidated `shareWhatChips` field.

--- test/page/multiplatform-page.ts
+++ test/page/multiplatform-page.ts
@@ -12,11 +12,7 @@ export class MultiplatformPage implements PageWithGlobalSearch {
     readonly heroActionButon: Locator;
     readonly shareWhatBlock: Locator;
     readonly shareWhatTitle: Locator;
-    readonly shareWhatChipAnchor: Locator;
-    readonly shareWhatChipContent: Locator;
-    readonly shareWhatChipContentTitle: Locator;
-    readonly shareWhatChipContentText: Locator;
-    readonly shareWhatChipContentAction: Locator;
+    readonly shareWhatChips: Locator;
     readonly ctaBlockTitle: Locator;
     readonly ctaBlockAction: Locator;
 
@@ -31,11 +27,7 @@ export class MultiplatformPage implements PageWithGlobalSearch {
         this.heroActionButon = page.getByTestId('hero-action-button');
         this.shareWhatBlock = page.getByTestId('share-what-block');
         this.shareWhatTitle = page.getByTestId('share-what-title');
-        this.shareWhatChipAnchor = page.getByTestId('share-what-chip-anchor');
-        this.shareWhatChipContent = page.getByTestId('share-what-chip-content');
-        this.shareWhatChipContentTitle = page.getByTestId('share-what-chip-content-title');
-        this.shareWhatChipContentText = page.getByTestId('share-what-chip-content-text');
-        this.shareWhatChipContentAction = page.getByTestId('share-what-chip-content-action');
+        this.shareWhatChips = page.getByTestId('share-what-chip-anchor');
         this.ctaBlockTitle = page.getByTestId('cta-block-title');
         this.ctaBlockAction = page.getByTestId('cta-block-action');
     }
