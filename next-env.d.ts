f20187698e9a56b316c70c2306b286be98088a9a
zoobestik <kb.chernenko@gmail.com>
2025-12-18 18:38:57 +0100

    feat(build): update Next.js to v14.2.35 and dependencies
    - Upgrade `next` to `v14.2.35` and align related dependencies (e.g., `@next/env`, `@next/swc-*`, `@swc/helpers`).
    - Update `yarn.lock` to reflect dependency changes.
    - Simplify npm script `next-build-static` by removing the `next export` step.
    - Update TypeScript reference link in `next-env.d.ts` to the latest documentation.

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,4 +1,4 @@
 /// <reference types="next" />
 
 // NOTE: This file should not be edited
-// see https://nextjs.org/docs/basic-features/typescript for more information.
+// see https://nextjs.org/docs/pages/building-your-application/configuring/typescript for more information.
