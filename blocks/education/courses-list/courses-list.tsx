382bb708a077551f4786ba67e233eabaae0b15c2
Yuri Teplyakov <yuri.teplyakov@jetbrains.com>
2026-01-23 13:08:18 +0100

    ktl-727 fix: refactor text styles

--- blocks/education/courses-list/courses-list.tsx
+++ blocks/education/courses-list/courses-list.tsx
@@ -23,16 +23,16 @@ export const CoursesList: FC<CoursesListProps> = ({ universities }) => {
     const textCn = useTextStyles();
     return (
         <div className={styles.list}>
-            <div className={styles.header}>
-                <div className={cn(styles.cell, styles.cellFirst, textCn('rs-h4',))}>University title</div>
-                <div className={cn(styles.cell, styles.cellSecond, textCn('rs-h4',))}>Location</div>
-                <div className={cn(styles.cell, styles.cellThird, textCn('rs-h4',))}>Teaching Kotlin</div>
+            <div className={cn(styles.header, textCn('rs-h4'))}>
+                <div className={cn(styles.cell, styles.cellFirst)}>University title</div>
+                <div className={cn(styles.cell, styles.cellSecond)}>Location</div>
+                <div className={cn(styles.cell, styles.cellThird)}>Teaching Kotlin</div>
             </div>
             {universities.map((university) => (
-                <div className={styles.item} key={university.id}>
-                    <div className={cn(styles.cell, styles.cellFirst, textCn('rs-text-2', { hardness: 'hard' }))}>{university.title}</div>
-                    <div className={cn(styles.cell, styles.cellSecond, textCn('rs-text-2', { hardness: 'hard' }))}>{university.location}</div>
-                    <div className={cn(styles.cell, styles.cellThird, textCn('rs-text-2', { hardness: 'hard' }))}>
+                <div className={cn(styles.item, textCn('rs-text-2', { hardness: 'hard' }))} key={university.id}>
+                    <div className={cn(styles.cell, styles.cellFirst)}>{university.title}</div>
+                    <div className={cn(styles.cell, styles.cellSecond)}>{university.location}</div>
+                    <div className={cn(styles.cell, styles.cellThird)}>
                         {university.courses.map((course) => (
                             <a className={textCn('rs-link', { external: true, hardness: 'hard' })} href={course.url} target="_blank" rel="noopener noreferrer" key={`${course.name}-${course.url}`}>
                                 {course.name}
