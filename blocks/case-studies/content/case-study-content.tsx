93514de36030502c9150eea04b2e5210d87fe889
berezinant <berezinant@gmail.com>
2026-01-26 12:13:12 +0100

    feat(ktl-4083): added frontmatter info to case page

--- blocks/case-studies/content/case-study-content.tsx
+++ blocks/case-studies/content/case-study-content.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import cn from 'classnames';
+import Link from 'next/link';
 import { useTextStyles } from '@rescui/typography';
 import { Markdown } from '../../../utils/mdToHtml';
 import styles from './case-study-content.module.css';
@@ -22,6 +23,54 @@ export const CaseStudyContent: React.FC<CaseStudyContentProps> = ({ content, cla
             {frontmatter?.title && (
                 <h1 className={cn(textCn('rs-hero'), styles.title)}>{frontmatter.title}</h1>
             )}
+
+            {frontmatter && (
+                <div className={cn(styles.infoSection, textCn('rs-text-2'))}>
+                    <div className={styles.infoBlock}>
+                        <div>
+                            <span className={styles.infoLabel}>Company:</span>{' '}
+                            <span className={styles.infoValue}>{frontmatter.title}</span>
+                        </div>
+                        {frontmatter.industry && (
+                            <div>
+                                <span className={styles.infoLabel}>Industry:</span>{' '}
+                                <span className={styles.infoValue}>{frontmatter.industry}</span>
+                            </div>
+                        )}
+                    </div>
+                    <div className={styles.infoBlock}>
+                        {frontmatter.size && (
+                            <div>
+                                <span className={styles.infoLabel}>Size:</span>{' '}
+                                <span className={styles.infoValue}>{frontmatter.size}</span>
+                            </div>
+                        )}
+                        {frontmatter.usedProductTitle && (
+                            <div>
+                                <span className={styles.infoLabel}>JetBrains products used:</span>{' '}
+                                <span className={styles.infoValue}>
+                                    {frontmatter.usedProductLink ? (
+                                        <Link href={frontmatter.usedProductLink}>
+                                            {frontmatter.usedProductTitle}
+                                        </Link>
+                                    ) : (
+                                        frontmatter.usedProductTitle
+                                    )}
+                                </span>
+                            </div>
+                        )}
+                    </div>
+                    <div className={styles.infoBlock}>
+                        {frontmatter.country && (
+                            <div>
+                                <span className={styles.infoLabel}>Country:</span>{' '}
+                                <span className={styles.infoValue}>{frontmatter.country}</span>
+                            </div>
+                        )}
+                    </div>
+                </div>
+            )}
+
             {frontmatter?.coverImg && (
                 <img className={styles.coverImg} src={frontmatter.coverImg} alt={frontmatter.title} />
             )}
