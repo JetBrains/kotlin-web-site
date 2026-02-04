e0169c2d09e8b4c563bbcd6c413ec470ed0a9469
zoobestik <kb.chernenko@gmail.com>
2026-02-04 14:51:54 +0100

    fix(ci): update Android SDK setup logic for ComposeMultiplatformCore build script

--- .teamcity/references/builds/compose/ComposeMultiplatformCore.kt
+++ .teamcity/references/builds/compose/ComposeMultiplatformCore.kt
@@ -23,13 +23,60 @@ private fun BuildType.additionalParams(): BuildType = apply {
     }
 }
 
-private fun BuildSteps.downloadAndroidSdk() = script {
+data class AndroidSdk(
+    val compileSdk: String,
+    val buildTools: String,
+    val cmdToolsVersion: String,
+)
+
+// Download Android SDK
+// https://code.jetbrains.team/p/ui/repositories/compose-teamcity-config/files/main/.teamcity/compose/scripts/downloadAndroidSdk.sh
+private fun BuildSteps.downloadAndroidSdk(androidSdk: AndroidSdk) = script {
     name = "Download Android SDK"
     // language=bash
     scriptContent = """
         #!/bin/bash
         set -euo pipefail
-        ./jbdeps/android-sdk/downloadAndroidSdk
+        
+        ANDROID_COMPILE_SDK=${androidSdk.compileSdk}
+        ANDROID_BUILD_TOOLS=${androidSdk.buildTools}
+        ANDROID_CMD_TOOLS_VERSION=${androidSdk.cmdToolsVersion}
+        
+        downloadLinuxSDK() {
+            curl --silent --show-error -o commandlinetools-linux.zip https://dl.google.com/android/repository/commandlinetools-linux-${'$'}{ANDROID_CMD_TOOLS_VERSION}_latest.zip
+            unzip -o -d android-sdk-linux commandlinetools-linux.zip
+            rm commandlinetools-linux.zip
+            echo y | "android-sdk-linux/cmdline-tools/bin/sdkmanager" --sdk_root=android-sdk-linux "platform-tools" "platforms;android-${'$'}ANDROID_COMPILE_SDK" "build-tools;${'$'}ANDROID_BUILD_TOOLS"
+            echo y | "android-sdk-linux/cmdline-tools/bin/sdkmanager" --sdk_root=android-sdk-linux --licenses > /dev/null
+            export ANDROID_HOME=${'$'}PWD/android-sdk-linux
+        }
+        
+        downloadMacOsSDK() {
+            curl --silent --show-error -o commandlinetools-mac.zip https://dl.google.com/android/repository/commandlinetools-mac-${'$'}{ANDROID_CMD_TOOLS_VERSION}_latest.zip
+            unzip -o -d android-sdk-darwin commandlinetools-mac.zip
+            rm commandlinetools-mac.zip
+            echo y | "android-sdk-darwin/cmdline-tools/bin/sdkmanager" --sdk_root=android-sdk-darwin "platform-tools" "platforms;android-${'$'}ANDROID_COMPILE_SDK" "build-tools;${'$'}ANDROID_BUILD_TOOLS"
+            echo y | "android-sdk-darwin/cmdline-tools/bin/sdkmanager" --sdk_root=android-sdk-darwin --licenses > /dev/null
+            export ANDROID_HOME=${'$'}PWD/android-sdk-darwin
+        }
+        
+        if [[ "${'$'}OSTYPE" == "linux-gnu"* ]]; then
+            downloadLinuxSDK
+        elif [[ "${'$'}OSTYPE" == "darwin"* ]]; then
+            downloadMacOsSDK
+        elif [[ "${'$'}OSTYPE" == "cygwin" ]]; then
+            echo "Please download Android SDK manually (https://developer.android.com/studio)"
+        elif [[ "${'$'}OSTYPE" == "msys" ]]; then
+            echo "Please download Android SDK manually (https://developer.android.com/studio)"
+        elif [[ "${'$'}OSTYPE" == "win32" ]]; then
+            echo "Please download Android SDK manually (https://developer.android.com/studio)"
+        else
+            echo "Unknown OS"
+        fi
+        
+        # Export ANDROID_HOME as for other build steps
+        echo "ANDROID_HOME=${'$'}ANDROID_HOME"
+        echo "##teamcity[setParameter name='env.ANDROID_HOME' value='${'$'}ANDROID_HOME']"
     """.trimIndent()
     formatStderrAsError = true
 }
@@ -51,7 +98,7 @@ class ComposeMultiplatformCore(init: ComposeMultiplatformCore.() -> Unit) : Refe
         }
     }
 
-    fun addVersion(version: String, tagOrBranch: String) = addReference(version) {
+    fun addVersion(version: String, tagOrBranch: String, androidSdk: AndroidSdk) = addReference(version) {
         apiReference(
             version,
             gitUrl = "git@github.com:JetBrains/compose-multiplatform-core.git",
@@ -59,7 +106,7 @@ class ComposeMultiplatformCore(init: ComposeMultiplatformCore.() -> Unit) : Refe
             templateDir = "mpp/apiReferences/dokka-templates",
             pagesDir = "out/androidx/mpp/apiReferences/build/dokka/html",
             steps = {
-                downloadAndroidSdk()
+                downloadAndroidSdk(androidSdk)
                 dokkaBuildHtml(version) {
                     tasks = ":mpp:apiReferences:buildApiReferencesWithStories"
                     gradleParams += " -PapiReferences.storiesRootPath=/api/${urlPart}/stories"
