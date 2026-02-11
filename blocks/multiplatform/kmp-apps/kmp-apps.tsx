f8382f9aab458168878b606eafd2c4245b927bf1
Yuri Teplyakov <yuri.teplyakov@jetbrains.com>
2026-02-11 17:41:22 +0100

    ktl-4105 feat: style updates

--- blocks/multiplatform/kmp-apps/kmp-apps.tsx
+++ blocks/multiplatform/kmp-apps/kmp-apps.tsx
@@ -5,19 +5,22 @@ import Img from 'next/image';
 import styles from './kmp-apps.module.css';
 import kmpAppsImage from './images/kmp-apps.webp';
 
+import { useMS } from '@jetbrains/kotlin-web-site-ui/out/components/breakpoints-v2';
+
 export const KMPApps = () => {
     const textCn = createTextCn('dark');
+    const isMS = useMS();
 
     return (
         <div id="kmp-apps" className={cn(styles.section, 'ktl-layout ktl-layout--center')}>
             <div className={styles.wrapper}>
-                <h2 className={cn(textCn('rs-h2'), styles.title)}>
+                <h2 className={cn(textCn(isMS ? 'rs-h3' : 'rs-h2'), styles.title)}>
                     KMP presence among the top 10K apps doubled year over year.
                 </h2>
                 <div className={styles.imageWrapper}>
                     <Img
                         src={kmpAppsImage}
-                        alt="KMP apps statistics"
+                        alt="KMP apps"
                         className={styles.image}
                     />
                 </div>
