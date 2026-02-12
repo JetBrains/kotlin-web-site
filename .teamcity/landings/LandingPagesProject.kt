d222fca56cef045d6747c892d17bb5c1d12aea6e
nikpachoo <Nikolay.Pachkov@jetbrains.com>
2026-02-12 20:00:53 +0100

    KTL-4149 feat: add deployment build types for staging and production

--- .teamcity/landings/LandingPagesProject.kt
+++ .teamcity/landings/LandingPagesProject.kt
@@ -2,6 +2,8 @@ package landings
 
 import jetbrains.buildServer.configs.kotlin.Project
 import landings.builds.BuildLandingPage
+import landings.builds.DeployLandingToStaging
+import landings.builds.DeployLandingToProduction
 
 object LandingPagesProject : Project({
   name = "Landing Pages"
@@ -11,6 +13,8 @@ object LandingPagesProject : Project({
     vcsRoot(createVcsRootForLanding(config))
 
     buildType(BuildLandingPage(config))
+    buildType(DeployLandingToStaging(config))
+    buildType(DeployLandingToProduction(config))
   }
 })
 
