ddda4bf08ef1a581c930376942b69d846a56316c
berezinant <berezinant@gmail.com>
2026-02-13 12:18:07 +0100

    feat(ktl-3977): use resc links in md2html

--- utils/mdToHtml.tsx
+++ utils/mdToHtml.tsx
@@ -1,8 +1,27 @@
 import { ComponentProps } from 'react';
+import { useTextStyles } from '@rescui/typography';
 import MD2Markdown from 'markdown-to-jsx';
 
 export type MarkdownProps = ComponentProps<typeof MD2Markdown>;
 
 export function Markdown(props: MarkdownProps) {
-    return <MD2Markdown {...props} />;
+    const textCn = useTextStyles();
+    const linkClass = textCn('rs-link');
+    const overriddenClassNamesForTags = {
+        a: {
+            props: {
+                className: linkClass
+            }
+        }
+    };
+    return <MD2Markdown
+        {...props}
+        options={{
+            ...props.options,
+            overrides: {
+                ...props.options?.overrides,
+                ...overriddenClassNamesForTags
+            }
+        }}
+    />;
 }
