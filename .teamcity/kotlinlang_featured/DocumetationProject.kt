09a66c6ffa0ae3a55a5108780e0b6e90334e1e18
andrei.ezerskii <andrei.ezerskii@jetbrains.com>
2026-04-24 14:12:43 +0200

    Add documentation VCS roots

--- .teamcity/kotlinlang_featured/DocumetationProject.kt
+++ .teamcity/kotlinlang_featured/DocumetationProject.kt
@@ -1,7 +1,20 @@
 package kotlinlang_featured
 
 import jetbrains.buildServer.configs.kotlin.Project
+import kotlinlang_featured.vcsRoots.documentation.KotlinMultiplatformRoot
+import kotlinlang_featured.vcsRoots.documentation.KotlinReferenceRoot
+import kotlinlang_featured.vcsRoots.documentation.KotlinxCoroutinesRoot
+import kotlinlang_featured.vcsRoots.documentation.DokkaRoot
+import kotlinlang_featured.vcsRoots.documentation.APIGuidelinesRoot
+import kotlinlang_featured.vcsRoots.documentation.KotlinxLincheckRoot
 
 object DocumentationProject: Project ({
     name = "Documentation Featured"
+
+    vcsRoot(KotlinMultiplatformRoot)
+    vcsRoot(KotlinReferenceRoot)
+    vcsRoot(KotlinxCoroutinesRoot)
+    vcsRoot(KotlinxLincheckRoot)
+    vcsRoot(DokkaRoot)
+    vcsRoot(APIGuidelinesRoot)
 })
---
+++ .teamcity/kotlinlang_featured/vcsRoots/documentation/DocumentationRoots.kt
@@ -0,0 +1,65 @@
+package kotlinlang_featured.vcsRoots.documentation
+
+import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
+
+// Kotlin Multiplatform Root
+object KotlinMultiplatformRoot: GitVcsRoot({
+    name = "Kotlin Multiplatform Root"
+    url = "git@github.com:JetBrains/kotlin-multiplatform-dev-docs.git"
+    branch = "refs/heads/master"
+    branchSpec = "+:refs/heads/*"
+    authMethod = uploadedKey {
+        uploadedKey = "default teamcity key"
+    }
+})
+
+// Kotlin Reference Root
+object KotlinReferenceRoot: GitVcsRoot ({
+    name = "Kotlin Reference"
+    url = "git@github.com:JetBrains/kotlin-web-site.git"
+    branch = "refs/heads/master"
+    branchSpec = "+:refs/heads/*"
+    authMethod = uploadedKey {
+        uploadedKey = "default teamcity key"
+    }
+})
+
+object KotlinxCoroutinesRoot: GitVcsRoot ({
+    name = "Kotlinx Coroutines"
+    url = "git@github.com:Kotlin/kotlinx.coroutines.git"
+    branch = "refs/heads/master"
+    branchSpec = "+:refs/heads/*"
+    authMethod = uploadedKey {
+        uploadedKey = "default teamcity key"
+    }
+})
+
+object KotlinxLincheckRoot: GitVcsRoot ({
+    name = "Kotlinx Lincheck"
+    url = "git@github.com:JetBrains/lincheck.git"
+    branch = "refs/heads/master"
+    branchSpec = "+:refs/heads/*"
+    authMethod = uploadedKey {
+        uploadedKey = "default teamcity key"
+    }
+})
+
+object DokkaRoot: GitVcsRoot ({
+    name = "Dokka"
+    url = "git@github.com:Kotlin/dokka.git"
+    branch = "refs/heads/master"
+    branchSpec = "+:refs/heads/*"
+    authMethod = uploadedKey {
+        uploadedKey = "default teamcity key"
+    }
+})
+
+object APIGuidelinesRoot: GitVcsRoot ({
+    name = "API Guidelines"
+    url = "git@github.com:Kotlin/api-guidelines.git"
+    branch = "refs/heads/main"
+    branchSpec = "+:refs/heads/*"
+    authMethod = uploadedKey {
+        uploadedKey = "default teamcity key"
+    }
+})
