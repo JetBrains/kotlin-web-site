76a368d6c07c317d9d410717c2b0b785e6c41ddc
andrei.ezerskii <andrei.ezerskii@jetbrains.com>
2026-04-24 14:17:36 +0200

    Add site project vcs roots

--- .teamcity/kotlinlang_featured/SiteProject.kt
+++ .teamcity/kotlinlang_featured/SiteProject.kt
@@ -1,7 +1,14 @@
 package kotlinlang_featured
 
 import jetbrains.buildServer.configs.kotlin.Project
+import kotlinlang_featured.vcsRoots.kotlinlang.GrammarGeneratorRoot
+import kotlinlang_featured.vcsRoots.kotlinlang.KotlinSpecRoot
+import kotlinlang_featured.vcsRoots.kotlinlang.WebHelpRoot
 
 object SiteProject: Project({
     name = "Kotlinlang Featured"
+
+    vcsRoot(GrammarGeneratorRoot)
+    vcsRoot(WebHelpRoot)
+    vcsRoot(KotlinSpecRoot)
 })
---
+++ .teamcity/kotlinlang_featured/vcsRoots/kotlinlang/KotlinLangRoots.kt
@@ -0,0 +1,40 @@
+package kotlinlang_featured.vcsRoots.kotlinlang
+
+import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
+
+// Grammar Generator
+object GrammarGeneratorRoot: GitVcsRoot({
+    name = "Grammar Generator VCS root"
+    url = "ssh://git@github.com/Kotlin/kotlin-website-grammar-generator"
+    branch = "refs/heads/master"
+    authMethod = uploadedKey {
+        uploadedKey = "default teamcity key"
+    }
+})
+
+// Kotlin Specs
+object KotlinSpecRoot: GitVcsRoot({
+    name = "Kotlin Spec VCS root"
+    url = "ssh://git@github.com/Kotlin/kotlin-spec"
+    branch = "refs/heads/release"
+    authMethod = uploadedKey {
+        uploadedKey = "default teamcity key"
+    }
+})
+
+// Webhelp
+object WebHelpRoot : GitVcsRoot({
+    name = "webhelp"
+    url = "ssh://git@github.com/JetBrains/webhelp"
+    branch = "refs/heads/master"
+    branchSpec = """
+        refs/heads/(*)
+        refs/tags/(*)
+    """.trimIndent()
+    useTagsAsBranches = true
+    userNameStyle = UserNameStyle.FULL
+    checkoutPolicy = AgentCheckoutPolicy.USE_MIRRORS
+    authMethod = uploadedKey {
+        uploadedKey = "default teamcity key"
+    }
+})
