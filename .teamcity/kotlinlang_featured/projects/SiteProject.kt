0fa8571f57a8fb82762e6415bb65f1a5456a3b31
andrei.ezerskii <andrei.ezerskii@jetbrains.com>
2026-05-03 13:57:24 +0200

    Remove unused WebHelpRoot from SiteProject configuration

--- .teamcity/kotlinlang_featured/projects/SiteProject.kt
+++ .teamcity/kotlinlang_featured/projects/SiteProject.kt
@@ -3,12 +3,10 @@ package kotlinlang_featured.projects
 import jetbrains.buildServer.configs.kotlin.Project
 import kotlinlang_featured.vcsRoots.kotlinlang.GrammarGeneratorRoot
 import kotlinlang_featured.vcsRoots.kotlinlang.KotlinSpecRoot
-import kotlinlang_featured.vcsRoots.kotlinlang.WebHelpRoot
 
 object SiteProject: Project({
     name = "Kotlinlang Featured"
 
     vcsRoot(GrammarGeneratorRoot)
-    vcsRoot(WebHelpRoot)
     vcsRoot(KotlinSpecRoot)
 })
