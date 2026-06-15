ea569821b88e55cd3600071bd3fe1e33a65c16d6
Yuri Teplyakov <yuri.teplyakov@jetbrains.com>
2026-06-15 11:39:36 +0200

    ktl-4474 feat: video update

--- blocks/kotlin-effect/hero-screen/hero-video/hero-video.tsx
+++ blocks/kotlin-effect/hero-screen/hero-video/hero-video.tsx
@@ -3,9 +3,7 @@ import React, { FC } from 'react';
 import cn from 'classnames';
 
 import styles from './hero-video.module.css';
-import kotlinEffectHeroMp4Video from './converted/kotlin-effect-hero.h264.mp4';
 import kotlinEffectHeroWebmVideo from './converted/kotlin-effect-hero.vp9.webm';
-import kotlinEffectHeroMobileMp4Video from './converted/kotlin-effect-hero-mobile.h264.mp4';
 import kotlinEffectHeroMobileWebmVideo from './converted/kotlin-effect-hero-mobile.vp9.webm';
 
 const LOOP_START_SECONDS = 3;
@@ -14,20 +12,8 @@ export const HeroVideo: FC = () => {
     const loopFromStartPoint = (event: React.SyntheticEvent<HTMLVideoElement>) => {
         const video = event.currentTarget;
 
-        if (video.duration <= LOOP_START_SECONDS) {
-            return;
-        }
-
         video.currentTime = LOOP_START_SECONDS;
         void video.play();
-    };
-
-    const loopBeforeEnd = (event: React.SyntheticEvent<HTMLVideoElement>) => {
-        const video = event.currentTarget;
-
-        if (video.duration && video.currentTime >= video.duration) {
-            loopFromStartPoint(event);
-        }
     };
 
     return (
@@ -39,11 +25,9 @@ export const HeroVideo: FC = () => {
                 playsInline
                 preload="auto"
                 aria-hidden="true"
-                onTimeUpdate={loopBeforeEnd}
                 onEnded={loopFromStartPoint}
             >
-                {/*<source src={kotlinEffectHeroWebmVideo} type='video/webm; codecs="vp9"' />*/}
-                <source src={kotlinEffectHeroMp4Video} type='video/mp4; codecs="avc1.4d401f"' />
+                <source src={kotlinEffectHeroWebmVideo} type='video/webm; codecs="vp9"' />
             </video>
             <video
                 className={cn(styles.video, styles.videoMobile)}
@@ -55,7 +39,6 @@ export const HeroVideo: FC = () => {
                 aria-hidden="true"
             >
                 <source src={kotlinEffectHeroMobileWebmVideo} type='video/webm; codecs="vp9"' />
-                <source src={kotlinEffectHeroMobileMp4Video} type='video/mp4; codecs="avc1.4d401f"' />
             </video>
         </>
     );
