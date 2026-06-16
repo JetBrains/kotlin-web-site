95d184aacf538ca58aaf8839680c242701b45de8
zoobestik <kb.chernenko@gmail.com>
2026-06-16 23:44:42 +0200

    tmp; use my repo with correct structure

--- .teamcity/documentation/vcsRoots/DocumentationRoots.kt
+++ .teamcity/documentation/vcsRoots/DocumentationRoots.kt
@@ -40,7 +40,7 @@ object KotlinxCoroutinesRoot: GitVcsRoot ({
 
 object KotlinxLincheckRoot: GitVcsRoot ({
     name = "Kotlinx Lincheck"
-    url = "git@github.com:JetBrains/lincheck.git"
+    url = "git@github.com:zoobestik/lincheck.git"
     branch = "refs/heads/develop"
     branchSpec = "+:refs/heads/*"
     checkoutPolicy = AgentCheckoutPolicy.USE_MIRRORS
