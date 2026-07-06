5da5ad352505fb9e87059228d6febdebe3977506
nikpachoo <Nikolay.Pachkov@jetbrains.com>
2026-07-06 09:47:50 +0200

    ktl-4601 fix: remove obsolete test

--- test/production/solutions-tab.spec.ts
+++ test/production/solutions-tab.spec.ts
@@ -24,15 +24,6 @@ test.describe('Solutions tab', () => {
         expect(page.url()).toContain(`${baseURL}/backend/`);
     });
 
-    test('Click on "Data science" button should open the related page', async ({ page, baseURL }) => {
-        const dataScienceButton = page.locator(testSelector("header")).getByText('Data science');
-        await expect(dataScienceButton).toBeVisible();
-        await dataScienceButton.click();
-        // We need a timeout here because of redirect to another URL.
-        await page.waitForTimeout(2000);
-        expect(page.url()).toContain(`${baseURL}/docs/data-analysis-overview.html`);
-    });
-
     test('Click on "Android" button should open the related page', async ({ page, baseURL }) => {
         const androidButton = page.locator(testSelector("header")).getByText('Android').first();
         await expect(androidButton).toBeVisible();
