da38542c728fcf1cf1d6b3e42f9dbec901133db3
nikpachoo <Nikolay.Pachkov@jetbrains.com>
2026-07-21 12:50:53 +0200

    chore: update VCS root URLs and standardize uploaded keys usage

--- .teamcity/kotlinlang/vcsRoots/GrammarGenerator.kt
+++ .teamcity/kotlinlang/vcsRoots/GrammarGenerator.kt
@@ -4,7 +4,7 @@ import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
 
 object GrammarGenerator: GitVcsRoot({
   name = "Grammar Generator VCS root"
-  url = "ssh://git@github.com/Kotlin/kotlin-website-grammar-generator"
+  url = "git@github.com:Kotlin/kotlin-website-grammar-generator.git"
   branch = "refs/heads/master"
   authMethod = uploadedKey {
     uploadedKey = "default teamcity key"
