345dcfca994cc25a43920cb78978d13c18b08e7b
nikpachoo <Nikolay.Pachkov@jetbrains.com>
2026-07-28 16:13:02 +0200

    KTL-4761 refactor: clean up after elimination of python

--- src/github.py
+++
@@ -1,9 +0,0 @@
-def assert_valid_git_hub_url(edit_on_github_url: str, page_path: str):
-    # Do you like to include yet another organization? Thing twice :)
-
-    url_lower = edit_on_github_url.lower()
-
-    assert \
-        url_lower.startswith('https://github.com/'.lower()), \
-        'Check `edit_on_github_url` for `' + page_path + "` to be either `JetBrains` or `kotlin` "
-
