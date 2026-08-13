6553f7cf3cb5c0e4fd5527a6e383f7c931f10a1d
nikpachoo <Nikolay.Pachkov@jetbrains.com>
2026-08-13 17:13:28 +0200

    KTL-4260 chore: get hero button back

--- blocks/main/hero/hero-block.tsx
+++ blocks/main/hero/hero-block.tsx
@@ -15,6 +15,8 @@ import AIIcon from './images/icons/ai.svg';
 import AndroidIcon from './images/icons/android.svg';
 import KotlinIcon from './images/icons/Kotlin.svg';
 import { WhyKotlin } from '@/blocks/main/why-kotlin/why-kotlin';
+import { Button } from '@rescui/button';
+import Link from 'next/link';
 
 interface Props {
 }
@@ -24,26 +26,34 @@ export const HeroBlock: FC<Props> = ({}) => {
     const darkTextCn = createTextCn('dark');
 
     return (
-        <HeroLayout>
-            <div className={styles.grid} data-testid={'hero-block-main-page'}>
+        <HeroLayout data-testid={'hero-block-main-page'}>
+            <div className={styles.grid}>
                 <div className={styles.heading}>
                     <h1 className={cn(darkTextCn('rs-super-hero'), styles.heroText)}>Kotlin</h1>
                     <p className={cn(darkTextCn('rs-subtitle-1'), styles.subtitle)}>Concise. Multiplatform. Fun.</p>
-
                     <Developer />
+                    <Button
+                        href="/docs/getting-started.html"
+                        size="l"
+                        theme="light"
+                        data-testid={'hero-get-started-button'}
+                        className={styles.heroButton}
+                    >
+                        Get started
+                    </Button>
                 </div>
 
                 <div className={styles.navItems}>
-                    <a className={styles.navItem} href={'/multiplatform/'}>
+                    <Link prefetch={false} className={styles.navItem} href={'/multiplatform/'}>
                         <img src={KMPIcon.src} className={cn(styles.navIcon, styles.navIconKmp)} alt={`KMP icon`} />
                         <p className={darkTextCn('rs-text-2')}>Multiplatform</p>
                         <ArrowRightIcon className={styles.navArrow} />
-                    </a>
-                    <a className={styles.navItem} href={'/backend/'}>
+                    </Link>
+                    <Link prefetch={false} className={styles.navItem} href={'/backend/'}>
                         <img src={BackendIcon.src} className={styles.navIcon} alt={`Backend icon`} />
                         <p className={darkTextCn('rs-text-2')}>Backend</p>
                         <ArrowRightIcon className={styles.navArrow} />
-                    </a>
+                    </Link>
                     <a className={styles.navItem} href={'/docs/kotlin-ai-apps-development-overview.html'}>
                         <img src={AIIcon.src} className={styles.navIcon} alt={`AI icon`} />
                         <p className={darkTextCn('rs-text-2')}>AI</p>
