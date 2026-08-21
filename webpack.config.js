71f2ca26354d1bc4124a86ceadb9efbc0fba091e
nikpachoo <Nikolay.Pachkov@jetbrains.com>
2026-08-21 14:57:12 +0200

    ktl-4761 chore: clean up webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -9,10 +9,8 @@ module.exports = (params = {}) => {
   const isProduction = process.env.NODE_ENV === 'production';
   const isDevelopment = !isProduction;
   const env = isProduction ? 'production' : 'development';
-  const isDevServer = process.env.WEBPACK_SERVE === 'true';
   const sourcemaps = params.sourcemaps || isDevelopment;
 
-  const nextJSHost = 'localhost:3000';
   const indexName = params['index-name'] || process.env.INDEX_NAME || 'dev_KOTLINLANG';
 
   return {
@@ -24,13 +22,13 @@ module.exports = (params = {}) => {
       path: path.join(__dirname, '_assets'),
       publicPath: '/_assets/',
       filename: '[name].js',
-      clean: !isDevServer,
+      clean: true,
     },
 
     devtool: sourcemaps ? 'source-map' : false,
 
     mode: env,
-    bail: !isDevServer,
+    bail: true,
 
     resolve: {
       alias: {
@@ -142,25 +140,6 @@ module.exports = (params = {}) => {
         'process.env.NODE_ENV': JSON.stringify(env),
         'process.env.ALGOLIA_INDEX_NAME': JSON.stringify(process.env.ALGOLIA_INDEX_NAME)
       })
-    ].filter(Boolean),
-
-    devServer: {
-      port: 9000,
-      hot: true,
-      proxy: {
-        '/community/**': {
-          target: `http://${nextJSHost}`,
-          bypass: function (req) {
-            req.headers.host = nextJSHost;
-          }
-        },
-        '/_next/**': {
-          target: `http://${nextJSHost}`,
-          bypass: function (req) {
-            req.headers.host = nextJSHost;
-          }
-        }
-      }
-    }
+    ].filter(Boolean)
   };
 };
