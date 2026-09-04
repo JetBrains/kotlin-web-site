e0b791c10f457d5febf04262259cb75b1c928b97
nikpachoo <Nikolay.Pachkov@jetbrains.com>
2026-09-04 13:06:53 +0200

    KTL-4977 chore: update build configuration

--- .teamcity/landings/builds/DeployLanding.kt
+++ .teamcity/landings/builds/DeployLanding.kt
@@ -2,6 +2,7 @@ package landings.builds
 
 import jetbrains.buildServer.configs.kotlin.BuildType
 import jetbrains.buildServer.configs.kotlin.FailureAction
+import jetbrains.buildServer.configs.kotlin.RelativeId
 import jetbrains.buildServer.configs.kotlin.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.triggers.finishBuildTrigger
 import landings.LandingConfiguration
@@ -19,14 +20,14 @@ enum class LandingDeployTarget(
  * Deployment build type for deploying a landing page to staging or production.
  *
  * Staging is automatically triggered when the landing page build succeeds.
- * Production is automatically triggered after staging only if autoDeployToProduction = true,
- * and can always be triggered manually.
+ * Production is triggered by the same build, but only if autoDeployToProduction = true,
+ * otherwise it can only be triggered manually.
  */
 class DeployLanding(
   private val config: LandingConfiguration,
   private val target: LandingDeployTarget
 ) : BuildType({
-  id("deploy_landing_${target.idPrefix}_${config.id}")
+  id(idFor(config, target))
   name = "Deploy ${config.name} to ${target.displayName}"
 
   type = Type.DEPLOYMENT
@@ -44,7 +45,8 @@ class DeployLanding(
   if (target == LandingDeployTarget.STAGING || config.autoDeployToProduction) {
     triggers {
       finishBuildTrigger {
-        buildType = BuildLandingPage.idFor(config)
+        buildType = RelativeId(BuildLandingPage.idFor(config)).value
+        branchFilter = "+:<default>"
         successfulOnly = true
       }
     }
@@ -81,4 +83,9 @@ class DeployLanding(
       dockerPull = true
     }
   }
-})
+}) {
+  companion object {
+    fun idFor(config: LandingConfiguration, target: LandingDeployTarget) =
+      "deploy_landing_${target.idPrefix}_${config.id}"
+  }
+}
