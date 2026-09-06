408065eebada958ad49093ad1d03f1e8724fa799
nikpachoo <Nikolay.Pachkov@jetbrains.com>
2026-09-06 20:32:38 +0200

    KTL-4799 feat: layout page for state of kotlin 2026

---
+++ pages/state-of-kotlin-2026/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { LandingLayout } from '@/components/landing-layout/landing-layout';
+
+import cn from 'classnames';
+
+import styles from './index.module.css';
+
+export const STATE_OF_KOTLIN_2026_TITLE = 'State of Kotlin 2026';
+export const STATE_OF_KOTLIN_2026_URL = '/state-of-kotlin-2026/';
+
+function Index() {
+    return (
+        <LandingLayout
+            title="State of Kotlin 2026 | The Kotlin Report"
+            description="15 years of Kotlin data: adoption, backend, multiplatform, and AI. Read the highlights and download the full State of Kotlin 2026 report."
+            theme="light"
+            hideTopMenu
+            currentTitle={STATE_OF_KOTLIN_2026_TITLE}
+            currentUrl={STATE_OF_KOTLIN_2026_URL}
+            ogImageName={'state-of-kotlin-2026.jpg'}
+            dataTestId={'state-of-kotlin-2026-page'}
+        >
+            <div className={styles.wrapper}>
+
+                <div className={cn('ktl-layout-v2', 'ktl-layout--center', styles.content)}>
+                   TODO
+                </div>
+            </div>
+        </LandingLayout>
+    );
+}
+
+export default Index;
